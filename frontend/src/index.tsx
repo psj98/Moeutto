53ef7b31d59afb972df610de494083d4ae76effb
seoyoung81 <seosue0801@gmail.com>
2023-10-24 17:44:19 +0900

    Feat: 메인페이지 생성

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,7 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+
+// pages
 import Work from "./pages/Work";
+import MainPage from "./pages/MainPage"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
@@ -13,7 +16,7 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Work />} />
-        <Route path="/main" element={<App />} />
+        <Route path="/main" element={<MainPage />} />
         <Route path="/mycloset" element={<App />} />
         <Route path="/notmycloset" element={<App />} />
         <Route path="/calendar" element={<App />} />
