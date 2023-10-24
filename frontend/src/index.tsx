e50ee05b6242d7239401abb00744bcdffc12ad8e
s01k1m <sk618dev@gmail.com>
2023-10-24 16:44:09 +0900

    Init: 프론트엔드 스켈레톤 코드 업로드
    1. pnpm을 사용하여 typescript를 이용해 react 기본 프로젝트를 만들었음.
    2. 기본 라우터를 설정하였음.

---
+++ frontend/src/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import Work from "./pages/Work";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Work />} />
+        <Route path="/main" element={<App />} />
+        <Route path="/mycloset" element={<App />} />
+        <Route path="/notmycloset" element={<App />} />
+        <Route path="/calendar" element={<App />} />
+        <Route path="/mypage" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
