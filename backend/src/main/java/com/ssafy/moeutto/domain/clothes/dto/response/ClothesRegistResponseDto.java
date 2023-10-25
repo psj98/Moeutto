91705509d7cd844c8c64002853f4f9e3543a9362
park98sj1@hanmail.net <park98sj1@hanmail.net>
2023-10-25 16:02:06 +0900

    Feat: 옷 등록 구현

--- backend/src/main/java/com/ssafy/moeutto/domain/clothes/dto/response/ClothesRegistResponseDto.java
+++ backend/src/main/java/com/ssafy/moeutto/domain/clothes/dto/response/ClothesRegistResponseDto.java
@@ -1,47 +1,36 @@
-package com.ssafy.moeutto.domain.clothes.entity;
+package com.ssafy.moeutto.domain.clothes.dto.response;
 
 import com.ssafy.moeutto.domain.member.entity.Member;
 import com.ssafy.moeutto.domain.middleCategory.entity.MiddleCategory;
-import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
-import org.hibernate.annotations.DynamicInsert;
-import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.sql.Date;
 
-@Entity
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@DynamicInsert
-public class Clothes {
+@NoArgsConstructor
+public class ClothesRegistResponseDto {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotNull
     private Integer id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id")
+    @NotNull
     private Member member;
 
-    @OneToOne
-    @JoinColumn(name = "middle_category_id")
+    @NotNull
     private MiddleCategory middleCategory;
 
     @NotNull
-    @Column(length = 50)
     private String name;
 
     @NotNull
-    @Column(length = 4)
     private String season;
 
     @NotNull
-    @Column(length = 6)
     private String color;
 
     @NotNull
@@ -52,22 +41,21 @@ public class Clothes {
     @Column(length = 30)
     private String shop;
 
-    @Column(length = 100)
     private String textile;
 
-    @Column(columnDefinition = "INTEGER DEFAULT 0")
     private Integer frequency;
 
-    @Column(columnDefinition = "INTEGER DEFAULT 0")
     private Integer star;
 
     // 사진 url 추가
+
     private Date regDate;
 
     private Date recentDate;
 
     @Builder(toBuilder = true)
-    public Clothes(Integer id, Member member, MiddleCategory middleCategory, String name, String season, String color, Integer thickness, Integer price, String shop, String textile, Integer frequency, Integer star, Date regDate, Date recentDate) {
+
+    public ClothesRegistResponseDto(Integer id, Member member, MiddleCategory middleCategory, String name, String season, String color, Integer thickness, Integer price, String shop, String textile, Integer frequency, Integer star, Date regDate, Date recentDate) {
         this.id = id;
         this.member = member;
         this.middleCategory = middleCategory;
--- backend/src/main/java/com/ssafy/moeutto/domain/clothes/service/ClothesService.java
+++ backend/src/main/java/com/ssafy/moeutto/domain/clothes/service/ClothesService.java
@@ -1,4 +1,10 @@
 package com.ssafy.moeutto.domain.clothes.service;
 
+import com.ssafy.moeutto.domain.clothes.dto.request.ClothesRegistRequestDto;
+import com.ssafy.moeutto.domain.clothes.dto.response.ClothesRegistResponseDto;
+import com.ssafy.moeutto.global.response.BaseException;
+
 public interface ClothesService {
+
+    ClothesRegistResponseDto registClothes(ClothesRegistRequestDto clothesRegistRequestDto) throws BaseException;
 }
