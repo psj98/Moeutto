369d23e8069ec7a7899ba3543d8514af5f928659
park98sj1@hanmail.net <park98sj1@hanmail.net>
2023-10-25 09:42:45 +0900

    Feat: Member 구조 생성

---
+++ backend/src/main/java/com/ssafy/moeutto/domain/member/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.ssafy.moeutto.domain.member.repository;
+
+import com.ssafy.moeutto.domain.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface MemberRepository extends JpaRepository<Member, UUID> {
+}
