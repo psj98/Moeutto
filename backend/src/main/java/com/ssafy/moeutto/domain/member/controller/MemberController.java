447fa8e4ede4fb8bc4515e65800106a50a678ecc
hyeonwoo <hjk8596@naver.com>
2023-10-25 15:44:42 +0900

    Fix: MemberLoginService

--- backend/src/main/java/com/ssafy/moeutto/domain/member/controller/MemberController.java
+++ backend/src/main/java/com/ssafy/moeutto/domain/member/controller/MemberController.java
@@ -1,15 +1,47 @@
 package com.ssafy.moeutto.domain.member.controller;
 
-import com.ssafy.moeutto.domain.member.service.MemberService;
-import com.ssafy.moeutto.domain.middleCategory.service.MiddleCategoryService;
+import com.ssafy.moeutto.domain.member.service.MemberLoginService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+
+@Slf4j
 @RestController
-@RequestMapping("/members")
 @RequiredArgsConstructor
+@RequestMapping("/members")
 public class MemberController {
 
-    private final MemberService memberService;
+    private final MemberLoginService memberLoginService;
+
+    @ResponseBody
+    @GetMapping("login")
+    public String kakaoLogin(){
+        try{
+            // 인가 코드 발급
+            String code = "";
+
+            // URL에 포함된 code를 이용하여 Access Token 발급
+            String accessToken = memberLoginService.getKakaoAccessToken(code);
+
+            // Access Token을 이용하여 카카오 서버에서 유저정보(이메일,닉네임,프사) 받아오기
+            HashMap<String, Object> userInfo = memberLoginService.getUserInfo(accessToken);
+            System.out.println("Member Controller : "+ userInfo);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+//    // info 확인용 테스트 api
+//    @GetMapping("info")
+//    public String kakaoInfo(){
+//
+//        return "info";
+//    }
+
+
 }
--- backend/src/main/java/com/ssafy/moeutto/domain/member/service/MemberLoginService.java
+++ backend/src/main/java/com/ssafy/moeutto/domain/member/service/MemberLoginService.java
@@ -1,9 +1,10 @@
-package com.ssafy.moeutto.member.service;
+package com.ssafy.moeutto.domain.member.service;
 
 import java.util.HashMap;
 
 public interface MemberLoginService {
 
+    public String getKakaoPermissionCode();
     public String getKakaoAccessToken(String code);
 
     public HashMap<String, Object> getUserInfo(String accessToken);
