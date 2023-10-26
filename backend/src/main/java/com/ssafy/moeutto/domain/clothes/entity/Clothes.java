ba9c932266dea9fe7ea0d72d4e415e3184882071
27kanghan <27kanghan@naver.com>
2023-10-26 17:05:47 +0900

    Feat: Cloth Entity 내용 추가

--- backend/src/main/java/com/ssafy/moeutto/domain/clothes/entity/Clothes.java
+++ backend/src/main/java/com/ssafy/moeutto/domain/clothes/entity/Clothes.java
@@ -6,6 +6,9 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import java.sql.Timestamp;
+import java.util.UUID;
 
 @Entity
 @Getter
@@ -13,11 +16,50 @@ import javax.persistence.Id;
 public class Clothes {
 
     @Id
+    @NotNull
     private Integer id;
+    @NotNull
+    private UUID memberId;
+    @NotNull
+    private String middleCategoryId;
+    @NotNull
+    private String name;
+    @NotNull
+    private String season;
+    @NotNull
+    private String color;
+    @NotNull
+    private Integer thickness;
+    private Integer price;
+    @NotNull
+    private String shop;
+    @NotNull
+    private String textile;
+    private Integer frequency;
+    private Integer star;
     private String imageUrl;
+    private Timestamp regDate;
+    @NotNull
+    private Timestamp recentDate;
+
+
 
     @Builder(toBuilder = true)
-    public Clothes(Integer id) {
+    public Clothes(Integer id, UUID memberId, String middleCategoryId, String name, String season, String color, Integer thickness, Integer price, String shop, String textile, Integer frequency, Integer star, String imageUrl, Timestamp regDate, Timestamp recentDate) {
         this.id = id;
+        this.memberId = memberId;
+        this.middleCategoryId = middleCategoryId;
+        this.name = name;
+        this.season = season;
+        this.color = color;
+        this.thickness = thickness;
+        this.price = price;
+        this.shop = shop;
+        this.textile = textile;
+        this.frequency = frequency;
+        this.star = star;
+        this.imageUrl = imageUrl;
+        this.regDate = regDate;
+        this.recentDate = recentDate;
     }
 }
