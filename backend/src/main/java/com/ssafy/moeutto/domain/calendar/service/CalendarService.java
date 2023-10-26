ee33f9a861483dd88c6bad01b4b48374a929ccc5
27kanghan <27kanghan@naver.com>
2023-10-26 13:43:12 +0900

    Refactor: 토큰 검증 부분 구조 개선

--- backend/src/main/java/com/ssafy/moeutto/domain/calendar/service/CalendarService.java
+++ backend/src/main/java/com/ssafy/moeutto/domain/calendar/service/CalendarService.java
@@ -1,5 +1,6 @@
 package com.ssafy.moeutto.domain.calendar.service;
 
+import com.ssafy.moeutto.domain.calendar.dto.request.CalendarRegistRequestDto;
 import com.ssafy.moeutto.domain.calendar.dto.response.CalendarListResponseDto;
 import com.ssafy.moeutto.domain.calendar.entity.Calendar;
 import com.ssafy.moeutto.global.response.BaseException;
@@ -10,6 +11,6 @@ import java.util.UUID;
 
 public interface CalendarService {
 
-    void registMyOutfit(UUID memberId, String calendar) throws BaseException;;
+    void registMyOutfit(UUID memberId,CalendarRegistRequestDto requestDto) throws BaseException;;
     CalendarListResponseDto getCalendarList(UUID memberId, Timestamp curDate) throws BaseException;
 }
