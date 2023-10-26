f342aa1ac0d87aa302324d808ee539f21686364f
s01k1m <sk618dev@gmail.com>
2023-10-26 15:00:52 +0900

    Feat: 사이드 메뉴바 생성 및 CSS
    사이드 메뉴바 생성하였고 CSS 완료함

--- frontend/src/pages/MyCloset.tsx
+++ frontend/src/pages/MyCloset.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 
-function Work() {
+function MyCloset() {
   return (
-    <div className="Work">
-      <div className="font-bold text-pink text-WebBody1">Mypage page</div>
+    <div className="myCloset">
+      <div className="font-bold text-pink text-WebBody1">My Closet page</div>
       <Link to="/main">메인 페이지</Link>
       <br />
       <Link to="/mycloset">나의 옷장</Link>
@@ -14,4 +14,4 @@ function Work() {
   );
 }
 
-export default Work;
+export default MyCloset;
