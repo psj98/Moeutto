55c83a58e0b4847438932c2441cc6e971c81414c
27kanghan <27kanghan@naver.com>
2023-10-26 15:46:30 +0900

    Feat: 착장 좋아요 여부 저장

--- backend/src/main/java/com/ssafy/moeutto/domain/calendar/service/CalendarService.java
+++ backend/src/main/java/com/ssafy/moeutto/domain/calendar/service/CalendarService.java
@@ -1,6 +1,7 @@
 package com.ssafy.moeutto.domain.calendar.service;
 
 import com.ssafy.moeutto.domain.calendar.dto.request.CalendarRegistRequestDto;
+import com.ssafy.moeutto.domain.calendar.dto.request.CalendarScoreRequestDto;
 import com.ssafy.moeutto.domain.calendar.dto.response.CalendarListResponseDto;
 import com.ssafy.moeutto.domain.calendar.entity.Calendar;
 import com.ssafy.moeutto.global.response.BaseException;
@@ -15,4 +16,6 @@ public interface CalendarService {
     CalendarListResponseDto getCalendarList(UUID memberId, Timestamp curDate) throws BaseException;
 
     void deleteCalendar(UUID memberId, Integer id) throws BaseException;
+
+    void scoreOutfit(UUID memberId, CalendarScoreRequestDto requestDto) throws BaseException;
 }
