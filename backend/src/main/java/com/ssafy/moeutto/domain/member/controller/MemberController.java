5a09bab6c12ff14757c95256c8052ed39e594340
hyeonwoo <hjk8596@naver.com>
2023-10-26 14:06:36 +0900

    Fix: MemberLoginServiceImpl, 카카오 accessToken 발급 완료

--- backend/src/main/java/com/ssafy/moeutto/domain/member/controller/MemberController.java
+++ backend/src/main/java/com/ssafy/moeutto/domain/member/controller/MemberController.java
@@ -20,20 +20,28 @@ public class MemberController {
     public String kakaoLogin(){
         try{
             // 인가 코드 발급
-            String code = "";
-
-            // URL에 포함된 code를 이용하여 Access Token 발급
-            String accessToken = memberLoginService.getKakaoAccessToken(code);
-
-            // Access Token을 이용하여 카카오 서버에서 유저정보(이메일,닉네임,프사) 받아오기
-            HashMap<String, Object> userInfo = memberLoginService.getUserInfo(accessToken);
-            System.out.println("Member Controller : "+ userInfo);
+            String code = memberLoginService.getKakaoPermissionCode();
+//                String code = "BRFCHw1Yoq9KYwSt-TYTQb-AfrokNzbEsITNR18s0fevLksqClwoaNmcpMQKKw0eAAABi2m-do_mTYKY7N6ACw";
 
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        return null;
+        return "Permission OK";
+    }
+
+    @ResponseBody
+    @PostMapping("check")
+    public String kakaoCheck(@RequestParam(required = false) String code){
+
+        // URL에 포함된 code를 이용하여 Access Token 발급
+        String accessToken = memberLoginService.KakaoAccessToken(code);
+        System.out.println("accessToken : "+accessToken);
+        // Access Token을 이용하여 카카오 서버에서 유저정보(이메일,닉네임,프사) 받아오기
+//        HashMap<String, Object> userInfo = memberLoginService.getUserInfo(accessToken);
+//        System.out.println("Member Controller : "+ userInfo);
+
+        return "Token OK";
     }
 
 //    // info 확인용 테스트 api
