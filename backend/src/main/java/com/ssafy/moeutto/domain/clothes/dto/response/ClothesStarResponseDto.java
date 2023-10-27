d0d21737d546f127d66de507ffcaed18c5c4dbd2
park98sj1@hanmail.net <park98sj1@hanmail.net>
2023-10-27 13:33:07 +0900

    Feat: 옷 즐겨찾기 등록 / 해제 구현

---
+++ backend/src/main/java/com/ssafy/moeutto/domain/clothes/dto/response/ClothesStarResponseDto.java
@@ -0,0 +1,24 @@
+package com.ssafy.moeutto.domain.clothes.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+
+@Getter
+@NoArgsConstructor
+public class ClothesStarResponseDto {
+
+    @NotNull
+    private Integer id; // 옷 id
+
+    @NotNull
+    private Integer star; // 즐겨찾기 여부
+
+    @Builder(toBuilder = true)
+    public ClothesStarResponseDto(Integer id, Integer star) {
+        this.id = id;
+        this.star = star;
+    }
+}
