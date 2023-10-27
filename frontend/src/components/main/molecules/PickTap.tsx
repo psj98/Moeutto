9bfed778eb30165b28995ac3fb32469ffab9682d
seoyoung81 <seosue0801@gmail.com>
2023-10-27 14:53:49 +0900

    Design: 레이아웃 CSS

--- frontend/src/components/main/molecules/PickTap.tsx
+++ frontend/src/components/main/molecules/PickTap.tsx
@@ -7,13 +7,12 @@ const PickTap = () => {
     const navigate = useNavigate();
 
     return (
-        <>
-            <div className="flex">
-                <div className="bg-gray-button" onClick={() => navigate('/pickpick')}>
-                    <PickCloset />
-                </div>
+        <div className="flex mt-3 h-[28vh]">
+            <div className="bg-gray-button w-1/2 rounded-2xl shadow-md flex justify-center items-center min-w-[300px]" onClick={() => navigate('/pickpick')}>
+                <PickCloset />
             </div>
-        </>
+        </div>
+     
     )
 }
 
--- frontend/src/components/main/organisms/RecommendList.tsx
+++ frontend/src/components/main/organisms/RecommendList.tsx
@@ -8,7 +8,7 @@ const RecommendList = () => {
         <>
         <div className="flex gap-3">
              {recommendations.map((item, index) => (
-                <div className={`flex flex-col items-center border w-1/3 h-2/3 rounded-md shadow-md ${index === 0 ? 'border-pink-hot' : 'border-gray'} pb-4`}>
+                <div className={`flex flex-col items-center border w-1/3 h-2/3 rounded-2xl shadow-md ${index === 0 ? 'border-pink-hot' : 'border-gray'} pb-4 min-w-[200px] max-w-[300px]`}>
                     {index === 0 && 
                     <TodayBox />}
                     <div style={{ marginTop: index === 0 ? '-5px' : '31px' }}>
