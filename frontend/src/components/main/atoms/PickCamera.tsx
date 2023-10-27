88cb1270fa8b2368663600298db535f29ed5a796
seoyoung81 <seosue0801@gmail.com>
2023-10-27 09:19:23 +0900

    Feat: input 창을 숨기고, 카메라 버튼을 누르면 파일을 첨부할 수 있게 함

--- frontend/src/components/main/atoms/PickCamera.tsx
+++ frontend/src/components/main/atoms/PickCamera.tsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { ChangeEvent, useRef } from "react";
 
-const PickCamera = () => {
+interface PickCameraPropsType {
+    handleImageChange: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+
+const PickCamera: React.FC<PickCameraPropsType> = ({ handleImageChange }) => {
+    const fileInputRef = useRef(null);
+
+    const handleCameraIconClick = () => {
+        fileInputRef.current.click();
+      };
+
     return (
         <div>
-            <img src={`/images/camera3d.png`} alt="icon" className="w-32 h-32 hover:scale-105" />
+            <img src={`/images/camera3d.png`} alt="icon" className="w-32 h-32 hover:scale-105" onClick={handleCameraIconClick} />
             <div>찍어찍어 당장 너를 찍어</div>
+            <input 
+                type="file"
+                id="fileInput"
+                accept="image/*"
+                ref={fileInputRef}
+                className="hidden"
+                onChange={handleImageChange}
+            />
         </div>
     )
 }
