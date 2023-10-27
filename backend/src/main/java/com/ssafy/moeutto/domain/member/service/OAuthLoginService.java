aefde4619f0923ab493e1485e958ae478ed9f3e8
psj98 <park98sj1@hanmail.net>
2023-10-27 21:44:29 +0900

    Refactor: Member Entity에 BINARY(16) 추가 (저장 안되는 오류 체크), Member 관련 메서드 Prettier

--- backend/src/main/java/com/ssafy/moeutto/domain/member/service/OAuthLoginService.java
+++ backend/src/main/java/com/ssafy/moeutto/domain/member/service/OAuthLoginService.java
@@ -16,47 +16,46 @@ public class OAuthLoginService {
     private final MemberRepository memberRepository;
     private final AuthTokensGenerator authTokensGenerator;
 
-
     /**
      * 우리 서비스에 로그인 시키기
+     *
      * @param email
      * @param nickname
      * @return
      */
-    public AuthTokens login(String email, String nickname){
+    public AuthTokens login(String email, String nickname) {
         UUID memberId = findOrCreateMember(email, nickname);
-
         return authTokensGenerator.generate(memberId);
     }
 
-
     /**
      * 카카오 로그인 멤버가 이미 멤버면 로그인, 아니면 가입시키고 로그인
+     *
      * @param email
      * @param nickname
      * @return
      */
-    private UUID findOrCreateMember(String email, String nickname){
+    private UUID findOrCreateMember(String email, String nickname) {
 
         Member findMember = memberRepository.findMemberByEmail(email);
 
-        System.out.println("findMember : "+findMember);
+        System.out.println("findMember : " + findMember);
 
-        if(findMember == null){
+        if (findMember == null) {
             return newMember(email, nickname);
-        }else{
-            return findMember.getId();
         }
 
+        return findMember.getId();
     }
 
     /**
      * 멤버가 아니어서 가입시키는 메소드
+     *
      * @param email
      * @param nickname
      * @return
      */
-    private UUID newMember(String email, String nickname){
+    private UUID newMember(String email, String nickname) {
         Member regist = Member.builder()
                 .id(UUID.randomUUID())
                 .email(email)
@@ -65,5 +64,4 @@ public class OAuthLoginService {
 
         return memberRepository.save(regist).getId();
     }
-
 }
