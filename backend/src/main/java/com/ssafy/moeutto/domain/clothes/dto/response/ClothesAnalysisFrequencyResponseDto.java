fb385e820b2de801b30c23432eca8459e8212b9b
hyeonwoo <hjk8596@naver.com>
2023-10-27 17:21:37 +0900

    Feat: ClothesController

---
+++ backend/src/main/java/com/ssafy/moeutto/domain/clothes/dto/response/ClothesAnalysisFrequencyResponseDto.java
@@ -0,0 +1,12 @@
+//package com.ssafy.moeutto.domain.clothes.dto.response;
+//
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//@Data
+//@NoArgsConstructor
+//public class ClothesAnalysisFrequencyResponseDto {
+//
+//    List<ClothesFrequencyDto>
+//
+//}
---
+++ backend/src/main/java/com/ssafy/moeutto/domain/clothes/dto/response/ClothesFrequencyDto.java
@@ -0,0 +1,9 @@
+package com.ssafy.moeutto.domain.clothes.dto.response;
+
+public class ClothesFrequencyDto {
+
+    String middleCategoryId;
+    String largeCategoryId;
+
+
+}
