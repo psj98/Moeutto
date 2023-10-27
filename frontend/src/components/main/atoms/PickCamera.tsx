d628d38eb897bec90d6192ac2b43adbe86e513c2
seoyoung81 <seosue0801@gmail.com>
2023-10-27 09:13:56 +0900

    Feat: 컴포넌트 분리
    1. 기능이 다른 컴포넌트를 분리하여 다른 컴포넌트로 만들었음

---
+++ frontend/src/components/main/atoms/PickCamera.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PickCamera = () => {
+    return (
+        <div>
+            <img src={`/images/camera3d.png`} alt="icon" className="w-32 h-32 hover:scale-105" />
+            <div>찍어찍어 당장 너를 찍어</div>
+        </div>
+    )
+}
+
+export default PickCamera;
---
+++ frontend/src/components/main/atoms/PickCloset.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PickCloset = () => {
+    return (
+        <div>
+            <img src={`/images/closet3d.png`} alt="icon" className="w-28 h-28 hover:scale-105" />
+            <div>골라골라 옷장에서 골라</div>
+        </div>
+    )
+}
+
+export default PickCloset;
