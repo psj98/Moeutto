890796b63bb52150a3e56def7b359ac18d3e41db
s01k1m <sk618dev@gmail.com>
2023-10-30 15:14:33 +0900

    Feat: 착장 검사 페이지 생성 및 오늘의 날씨 카드
    착장 검사 페이지를 시작하였다. 오늘의 날씨 카드 컴포넌트를 완성하였다.

--- frontend/src/pages/AnalysisPage.tsx
+++ frontend/src/pages/AnalysisPage.tsx
@@ -1,5 +1,12 @@
+import ClothAnalysisTemplate from '../components/analysis/templates/ClothAnalysisTemplate';
+
 const AnalysisPage = () => {
-  return <div>착장검사 페이지</div>;
+  return (
+    <>
+      <div className="text-WebBody3">오늘 입은 옷은 과연 몇 점일까요?</div>
+      <ClothAnalysisTemplate />
+    </>
+  );
 };
 
 export default AnalysisPage;
