a44504b3cc5b115879ffd463209656ae3a38535c
seoyoung81 <seosue0801@gmail.com>
2023-10-30 20:06:03 +0900

    Feat: 옷장 분석 페이지 그래프 구현

---
+++ frontend/src/components/analysis/molecules/Graph.tsx
@@ -0,0 +1,15 @@
+import LinearBright from "../atoms/LinearBright";
+import LinearTemp from "../atoms/LinearTemp";
+import Coordinate from "../atoms/Coordinate";
+
+const Graph = () => {
+    return (
+        <>
+            <LinearTemp />
+            <LinearBright />
+            <Coordinate />
+        </>
+    )
+}
+
+export default Graph;
--- frontend/src/components/analysis/organisms/ClothAnalysisOrganism.tsx
+++ frontend/src/components/analysis/organisms/ClothAnalysisOrganism.tsx
@@ -1,7 +1,14 @@
 import TodayWeatherCard from '../molecules/TodayWeatherCard';
+import Graph from '../molecules/Graph';
 
 const ClothAnalysisOrganism = () => {
-  return <TodayWeatherCard idx={1} />; // api 연결되면 idx는 동적으로 변함
+  return (
+    <>
+      <TodayWeatherCard idx={1} /> 
+      {/* api 연결되면 idx는 동적으로 변함 */}
+      <Graph />
+    </>
+  )
 };
 
 export default ClothAnalysisOrganism;
