72e092f503901bd45fddcaa1f2180d8569fc623c
seoyoung81 <seosue0801@gmail.com>
2023-10-30 19:44:54 +0900

    Feat: 좌표평면 그래프 구현

--- frontend/src/components/analysis/atoms/Coordinate.tsx
+++ frontend/src/components/analysis/atoms/Coordinate.tsx
@@ -1,71 +1,42 @@
+import React from "react";
+
+const SeasonLabel = ({ imagePath, label }) => (
+    <div className="flex items-center">
+        <img src={imagePath} alt="" className="w-[40px] h-[40px]" />
+        <div className="text-WebBody2 p-2">{label}</div>
+    </div>
+);
+
+const SeasonDot = ({ position }) => (
+    <div
+        className="absolute rounded-full w-3 h-3 bg-black border-2 border-pink z-99 flex"
+        style={{
+            top: position.top,
+            left: position.left
+        }}
+    />
+);
+
 const Coordinate = () => {
+    const position = { top: '20%', left: '30%' };
+
     return (
-        <> 
         <div className="flex items-center justify-center">
             <div className="bg-gray-button h-[384px] w-[634px] flex justify-center items-center rounded-xl">
-                <div className="flex items-center">
-                    <img src={"/images/summar.png"} alt="" className="w-[40px] h-[40px]" />
-                    <div className="text-WebBody2 p-2">여름</div>
-                </div>
+                <SeasonLabel imagePath="/images/summar.png" label="여름" />
                 <div className="flex flex-col items-center">
-                    <div className="flex items-center">
-                        <img src={"/images/spring.png"} alt="" className="w-[45px] h-[45px]" />
-                        <div className="text-WebBody2 p-2">봄</div>
-                    </div>
+                    <SeasonLabel imagePath="/images/spring.png" label="봄" />
                     <div className="bg-white h-[200px] w-[276px] relative rounded-md">
-                        <div className="absolute top-1/2 left-0 border border-black w-full"></div>
-                        <div className="absolute top-0 left-1/2 border border-black h-full"></div>
+                        <div className="absolute top-1/2 left-0 border border-black w-full z-1" />
+                        <SeasonDot position={position} />
+                        <div className="absolute top-0 left-1/2 border border-black h-full z-1" />
                     </div>
-                    <div className="flex items-center">
-                        <img src={"/images/fall.png"} alt="" className="w-[40px] h-[40px]" />
-                        <div className="text-WebBody2 p-2">가을</div>
-                    </div>
+                    <SeasonLabel imagePath="/images/fall.png" label="가을" />
                 </div>
-                <div className="flex items-center">
-                    <img src={"/images/winter.png"} alt="" className="w-[40px] h-[40px]" />
-                    <div className="text-WebBody2 p-2">겨울</div>
-                </div>
+                <SeasonLabel imagePath="/images/winter.png" label="겨울" />
             </div>
         </div>
-        </>
-    )
+    );
 }
 
 export default Coordinate;
-
-// const SeasonInfo = ({ imgSrc, imgAlt, text }) => {
-//     return (
-//       <div className="flex items-center">
-//         <img src={imgSrc} alt={imgAlt} className="w-[40px] h-[40px]" />
-//         <div className="text-WebBody2 p-2">{text}</div>
-//       </div>
-//     );
-//   };
-  
-//   const Line = () => {
-//     return (
-//       <div className="bg-white h-[200px] w-[276px] relative rounded-md">
-//         <div className="absolute top-1/2 left-0 border border-black w-full"></div>
-//         <div className="absolute top-0 left-1/2 border border-black h-full"></div>
-//       </div>
-//     );
-//   };
-  
-//   const Coordinate = () => {
-//     return (
-//       <div className="flex items-center justify-center">
-//         <div className="bg-gray-button h-[384px] w-[634px] flex justify-center items-center rounded-xl">
-//           <SeasonInfo imgSrc="/images/summar.png" imgAlt="Summer Image" text="여름" />
-//           <div className="flex flex-col items-center">
-//             <SeasonInfo imgSrc="/images/spring.png" imgAlt="Spring Image" text="봄" />
-//             <Line />
-//             <SeasonInfo imgSrc="/images/fall.png" imgAlt="Fall Image" text="가을" />
-//           </div>
-//           <SeasonInfo imgSrc="/images/winter.png" imgAlt="Winter Image" text="겨울" />
-//         </div>
-//       </div>
-//     );
-//   };
-  
-//   export default Coordinate;
-  
