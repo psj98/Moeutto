b4aa6567214290773e2053b5189eaeba68210ed6
s01k1m <sk618dev@gmail.com>
2023-10-30 23:54:50 +0900

    Design: 컨텐츠의 padding 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,8 +6,7 @@ import Sidebar from './components/common/SideNav';
 const ContentContainer = styled.div`
   width: 100%;
   height: 100%;
-  padding-left: 30px; // nav bar와 컨텐츠 사이의 간격
-  padding-top: 10px; // 컨텐츠와 상단 사이의 간격
+  padding: 10px 30px;
   margin-left: 250px; // nav bar fixed 되었는데 컨텐츠 가리지 않도록
 `;
 
