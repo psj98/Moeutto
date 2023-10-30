bb3ed693612444254d53025f2fc3fb82ed88fa51
s01k1m <sk618dev@gmail.com>
2023-10-30 23:54:15 +0900

    Feat: 착장 검사 페이지의 총 점수 컴포넌트 프론트 완료
    api는 아직 연결되지 않았다.

--- frontend/src/components/analysis/organisms/ClothAnalysisOrganism.tsx
+++ frontend/src/components/analysis/organisms/ClothAnalysisOrganism.tsx
@@ -1,10 +1,11 @@
-import TodayWeatherCard from '../molecules/TodayWeatherCard';
-import TotalScoreSection from '../molecules/TotalSCoreSection';
+import TodayWeatherCard from '../molecules/TodayWeatherCard'; // 날씨 정보 카드
+import TotalScoreSection from '../molecules/TotalSCoreSection'; // 총 점수 막대 그래프
 
 const ClothAnalysisOrganism = () => {
+  // api 연결되면 idx는 동적으로 변함
   return (
     <>
-      <TodayWeatherCard idx={1} /> // api 연결되면 idx는 동적으로 변함
+      <TodayWeatherCard idx={1} />
       <TotalScoreSection />
     </>
   );
