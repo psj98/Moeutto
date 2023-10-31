8fd9f8ad6c74e007e419f9896573bffea042c03f
seoyoung81 <seosue0801@gmail.com>
2023-10-31 22:04:16 +0900

    Feat: 상세 페이지 버튼 생성

--- frontend/src/components/clothes/atoms/ClothesDetailImg.tsx
+++ frontend/src/components/clothes/atoms/ClothesDetailImg.tsx
@@ -18,7 +18,7 @@ const ClothesDetailImg = ({ imgUrl, star, setStar }: PropsType ) => {
                 <div className="relative">
                     <img 
                         src={imgUrl} alt="clothes" 
-                        className="w-[38vw] border rounded-xl " 
+                        className="w-[38vw] border rounded-3xl " 
                     />
                     <div className="absolute -top-3 left-[30vw] transform:translate(-50%,-50%)">
                         {star ? (
