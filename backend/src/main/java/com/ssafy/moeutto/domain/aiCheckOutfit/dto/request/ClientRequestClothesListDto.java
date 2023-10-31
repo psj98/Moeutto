2fc0b10cd9e489fddac833f05f034d75f12e93f0
hyeonwoo <hjk8596@naver.com>
2023-10-31 17:49:23 +0900

    Feat: AICheckOutfit

---
+++ backend/src/main/java/com/ssafy/moeutto/domain/aiCheckOutfit/dto/request/ClientRequestClothesListDto.java
@@ -0,0 +1,12 @@
+package com.ssafy.moeutto.domain.aiCheckOutfit.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ClientRequestClothesListDto {
+    int id;
+    String largeCategoryId;
+
+}
--- backend/src/main/java/com/ssafy/moeutto/domain/aiCheckOutfit/dto/request/PythonRequestClothesListItems.java
+++ backend/src/main/java/com/ssafy/moeutto/domain/aiCheckOutfit/dto/request/PythonRequestClothesListItems.java
@@ -1,10 +1,12 @@
 package com.ssafy.moeutto.domain.aiCheckOutfit.dto.request;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
+@NoArgsConstructor
 public class PythonRequestClothesListItems {
 
     String largeCategoryid;
@@ -14,5 +16,16 @@ public class PythonRequestClothesListItems {
     int thickness;
     String textile;
     int frequency;
+
+    @Builder(toBuilder = true)
+    public PythonRequestClothesListItems(String largeCategoryid, int clothesId, String season, String color, int thickness, String textile, int frequency){
+        this.largeCategoryid = largeCategoryid;
+        this.clothesId = clothesId;
+        this.season = season;
+        this.color = color;
+        this.thickness = thickness;
+        this.textile = textile;
+        this.frequency = frequency;
+    }
 
 }
