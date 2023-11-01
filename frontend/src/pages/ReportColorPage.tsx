221d5d7ca1baae030d56d9cd8b1aff3c2129f1bd
s01k1m <sk618dev@gmail.com>
2023-11-02 04:47:03 +0900

    Feat: 옷장 분석 페이지 컬러 리스트 컴포넌트
    일단 러프하게 만들었다

--- frontend/src/pages/ReportColorPage.tsx
+++ frontend/src/pages/ReportColorPage.tsx
@@ -4,18 +4,31 @@ import * as React from 'react';
 import ClosetReportTemplate from '../components/report/templates/ClosetReportTemplate';
 import { ColorAmountProp } from '../components/report/atoms/DonutChart';
 
+export interface AnalysisData {
+  myAnalysisColor: ColorAmountProp[];
+  otherAnalysisColor: ColorAmountProp[];
+}
+
 const myAnalysisColor: ColorAmountProp[] = [
   { color: 'red', amount: 2 },
-  { color: 'blue', amount: 22 },
+  { color: 'blue', amount: 10 },
+  { color: 'purple', amount: 22 },
 ];
+
+const otherAnalysisColor: ColorAmountProp[] = [
+  { color: 'red', amount: 2 },
+  { color: 'blue', amount: 22 },
+  { color: 'pink', amount: 2 },
+  { color: 'yellow', amount: 10 },
+];
+
+const analysisData: AnalysisData = { myAnalysisColor, otherAnalysisColor };
 
 function ReportColorPage() {
   return (
     <div className="myCloset">
       <div className="font-bold text-pink text-WebBody1">My Closet page</div>
-      {/* <AddClothFormTemplate /> */}
-
-      <ClosetReportTemplate colorProps={myAnalysisColor} />
+      <ClosetReportTemplate analysisData={analysisData} />
     </div>
   );
 }
