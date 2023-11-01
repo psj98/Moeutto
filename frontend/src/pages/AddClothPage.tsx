eaecd6a423a76899489edf72cff6c57e8d9c9585
seoyoung81 <seosue0801@gmail.com>
2023-11-02 00:11:07 +0900

    Feat: mycloset 상단 바 컴포넌트

--- frontend/src/pages/AddClothPage.tsx
+++ frontend/src/pages/AddClothPage.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 // import { Link } from 'react-router-dom';
 import AddClothFormTemplate from '../components/add/templates/AddClothFormTemplate';
+import MyClosetBar from '../components/common/MyClosetBar';
 
 function AddClothPage() {
   return (
     <div className="myCloset">
       <div className="font-bold text-pink text-WebBody1">My Closet page</div>
+      <MyClosetBar state={2} />
       <AddClothFormTemplate />
     </div>
   );
