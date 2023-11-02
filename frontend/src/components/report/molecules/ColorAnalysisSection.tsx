0f614d7a4702e90b8596db2047edaa02b1ba0180
s01k1m <sk618dev@gmail.com>
2023-11-02 10:10:26 +0900

    Feat: 가장 좋아하는 색 정보 추가
    데이터 정렬하여 인덱스 0인값 뽑아서 가장 좋아하는 색 알려준다.

--- frontend/src/components/report/molecules/ColorAnalysisSection.tsx
+++ frontend/src/components/report/molecules/ColorAnalysisSection.tsx
@@ -8,8 +8,8 @@ interface ColorReportProps {
 const ColorAnalysisSection = ({ owner, colorProps }: ColorReportProps) => {
   return (
     <div className="w-[80%] text-center my-4 mx-auto">
-      <div>{owner}의 옷장에는</div>
-      <DonutChart colorAmountArray={colorProps}></DonutChart>
+      <div>{owner} 옷장에는</div>
+      <DonutChart owner={owner} colorAmountArray={colorProps}></DonutChart>
     </div>
   );
 };
