ff88325b08e7c826ca775d721905696d3cbb3733
park98sj1@hanmail.net <park98sj1@hanmail.net>
2023-11-02 14:08:24 +0900

    Feat: 날씨에 따른 AI 착장 추천 구현 (AI 구현 후 연결 필요)

--- backend/src/main/java/com/ssafy/moeutto/domain/aiRecOutfit/controller/AiRecOutfitController.java
+++ backend/src/main/java/com/ssafy/moeutto/domain/aiRecOutfit/controller/AiRecOutfitController.java
@@ -1,7 +1,9 @@
 package com.ssafy.moeutto.domain.aiRecOutfit.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.ssafy.moeutto.domain.aiRecOutfit.dto.request.AiRecOutfitCombineByAIRequestDto;
 import com.ssafy.moeutto.domain.aiRecOutfit.dto.request.AiRecOutfitCombineRequestDto;
+import com.ssafy.moeutto.domain.aiRecOutfit.dto.response.AiRecOutfitCombineResponseDto;
 import com.ssafy.moeutto.domain.aiRecOutfit.service.AiRecOutfitService;
 import com.ssafy.moeutto.domain.member.auth.AuthTokensGenerator;
 import com.ssafy.moeutto.global.response.BaseException;
@@ -30,7 +32,8 @@ public class AiRecOutfitController {
      * @param aiRecOutfitCombineRequestDtoList
      * @return List<AiRecOutfitCombineResponseDto>
      */
-    @PostMapping("/combine")
+//    @PostMapping("/combine")
+    @PostMapping("/combine-python-back-front")
     public BaseResponse<Object> recommendOutfit(@RequestHeader(value = "accessToken", required = false) String token,
                                                 @RequestBody List<AiRecOutfitCombineRequestDto> aiRecOutfitCombineRequestDtoList) {
         try {
@@ -40,14 +43,61 @@ public class AiRecOutfitController {
 
             UUID memberId = authTokensGenerator.extractMemberId(token); // 사용자 체크
 
-//            aiRecOutfitService.recommendOutfit(memberId, aiRecOutfitCombineRequestDtoList);
-            AiRecOutfitCombineByAIRequestDto aiRecOutfitCombineByAIRequestDto = aiRecOutfitService.recommendOutfitTest(memberId, aiRecOutfitCombineRequestDtoList);
+            List<AiRecOutfitCombineResponseDto> aiRecOutfitCombineResponseDtoList = aiRecOutfitService.recommendOutfit(memberId, aiRecOutfitCombineRequestDtoList);
+            return baseResponseService.getSuccessResponse(aiRecOutfitCombineResponseDtoList);
+        } catch (BaseException e) {
+            return baseResponseService.getFailureResponse(e.status);
+        } catch (JsonProcessingException e) {
+            return baseResponseService.getFailureResponse(BaseResponseStatus.NOT_FOUND_MEMBER);
+        }
+    }
+
+    /**
+     * back to front test code
+     *
+     * @param token
+     * @param aiRecOutfitCombineRequestDtoList
+     * @return AiRecOutfitCombineByAIRequestDto
+     */
+    @PostMapping("/combine-back-front")
+    public BaseResponse<Object> recommendOutfitTest(@RequestHeader(value = "accessToken", required = false) String token,
+                                                    @RequestBody List<AiRecOutfitCombineRequestDto> aiRecOutfitCombineRequestDtoList) {
+        try {
+            if (token == null || token.equals("")) {
+                throw new BaseException(BaseResponseStatus.SESSION_EXPIRATION);
+            }
+
+            UUID memberId = authTokensGenerator.extractMemberId(token); // 사용자 체크
+
+            AiRecOutfitCombineByAIRequestDto aiRecOutfitCombineByAIRequestDto = aiRecOutfitService.recommendOutfitBackFrontTest(memberId, aiRecOutfitCombineRequestDtoList);
             return baseResponseService.getSuccessResponse(aiRecOutfitCombineByAIRequestDto);
         } catch (BaseException e) {
             return baseResponseService.getFailureResponse(e.status);
         }
-//        catch (JsonProcessingException e) {
-//            return baseResponseService.getFailureResponse(BaseResponseStatus.NOT_FOUND_MEMBER);
-//        }
+    }
+
+    /**
+     * Python & Back & Front Test Code
+     *
+     * @param token
+     * @param aiRecOutfitCombineRequestDtoList
+     * @return AiRecOutfitCombineByAIRequestDto
+     */
+//    @PostMapping("/combine-python-back-front")
+    @PostMapping("/combine")
+    public BaseResponse<Object> recommendOutfitTestPythonBackFront(@RequestHeader(value = "accessToken", required = false) String token,
+                                                    @RequestBody List<AiRecOutfitCombineRequestDto> aiRecOutfitCombineRequestDtoList) {
+        try {
+            if (token == null || token.equals("")) {
+                throw new BaseException(BaseResponseStatus.SESSION_EXPIRATION);
+            }
+
+            UUID memberId = authTokensGenerator.extractMemberId(token); // 사용자 체크
+
+            List<AiRecOutfitCombineResponseDto> aiRecOutfitCombineResponseDtoList = aiRecOutfitService.recommendOutfitBackPythonFrontTest(memberId, aiRecOutfitCombineRequestDtoList);
+            return baseResponseService.getSuccessResponse(aiRecOutfitCombineResponseDtoList);
+        } catch (BaseException e) {
+            return baseResponseService.getFailureResponse(e.status);
+        }
     }
 }
