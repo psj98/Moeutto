ea8995bb447966265370fd21395cd89b9dc6419c
park98sj1@hanmail.net <park98sj1@hanmail.net>
2023-11-02 11:12:26 +0900

    Feat: 날씨에 따른 AI 착장 추천 구현 (AI 구현 후 연결 필요)

--- backend/src/main/java/com/ssafy/moeutto/domain/aiRecOutfit/repository/AIRecOutfitRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.moeutto.domain.aiRecOutfit.repository;
-
-import com.ssafy.moeutto.domain.aiRecOutfit.entity.AIRecOutfit;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AIRecOutfitRepository extends JpaRepository<AIRecOutfit, Integer> {
-}
