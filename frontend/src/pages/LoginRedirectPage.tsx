452d9f871edc23be9d230933a6da6c211cf416e5
27kanghan <27kanghan@naver.com>
2023-11-02 16:04:43 +0900

    Feat: 로그인 진행중 2

---
+++ frontend/src/pages/LoginRedirectPage.tsx
@@ -0,0 +1,34 @@
+// import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { defaultInstance } from '../api/api';
+
+export interface LoginRedirect {
+  code: String;
+  response: String;
+}
+
+const LoginRedirectPage = () => {
+  const navigate = useNavigate();
+  const code = new URL(window.location.href).searchParams.get('code');
+  const axiosInstance = defaultInstance();
+
+  const kakaoLogin = async () => {
+    const response = await axiosInstance.post(`/members/check?code=${code}`);
+
+    console.log(response);
+
+    if (response.data !== null) {
+      navigate('/main');
+    }
+  };
+
+  kakaoLogin();
+
+  return (
+    <div>
+      <h1>로그인중입니다.</h1>
+    </div>
+  );
+};
+
+export default LoginRedirectPage;
