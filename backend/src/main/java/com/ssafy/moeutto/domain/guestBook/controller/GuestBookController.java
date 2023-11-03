09230d0465f7fe2ac94f9de5afbb3e8586a74816
park98sj1@hanmail.net <park98sj1@hanmail.net>
2023-11-03 14:21:03 +0900

    Feat: 방명록 등록 구현
    Former-commit-id: 3d6e9799536ae376507fdfbe90ccabea5d47ee73

--- backend/src/main/java/com/ssafy/moeutto/domain/guestBook/controller/GuestBookController.java
+++ backend/src/main/java/com/ssafy/moeutto/domain/guestBook/controller/GuestBookController.java
@@ -1,9 +1,17 @@
 package com.ssafy.moeutto.domain.guestBook.controller;
 
+import com.ssafy.moeutto.domain.guestBook.dto.request.GuestBookRegistRequestDto;
+import com.ssafy.moeutto.domain.guestBook.dto.response.GuestBookRegistResponseDto;
 import com.ssafy.moeutto.domain.guestBook.service.GuestBookService;
+import com.ssafy.moeutto.domain.member.auth.AuthTokensGenerator;
+import com.ssafy.moeutto.global.response.BaseException;
+import com.ssafy.moeutto.global.response.BaseResponse;
+import com.ssafy.moeutto.global.response.BaseResponseService;
+import com.ssafy.moeutto.global.response.BaseResponseStatus;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/guestbooks")
@@ -11,4 +19,32 @@ import org.springframework.web.bind.annotation.RestController;
 public class GuestBookController {
 
     private final GuestBookService guestBookService;
+    private final BaseResponseService baseResponseService;
+    private final AuthTokensGenerator authTokensGenerator;
+
+    /**
+     * 방명록을 등록합니다.
+     *
+     * @param token
+     * @param guestBookRegistRequestDto
+     * @return
+     */
+    @PostMapping("")
+    public BaseResponse<Object> registGuestBook(@RequestHeader(value = "accessToken", required = false) String token,
+                                                @RequestBody GuestBookRegistRequestDto guestBookRegistRequestDto) {
+        try {
+            // 토큰 정보 체크
+            if (token == null || token.equals("")) {
+                throw new BaseException(BaseResponseStatus.SESSION_EXPIRATION);
+            }
+
+            UUID memberId = authTokensGenerator.extractMemberId(token); // 사용자 체크
+
+            // 방명록 등록
+            GuestBookRegistResponseDto guestBookRegistResponseDto = guestBookService.registGuestBook(memberId, guestBookRegistRequestDto);
+            return baseResponseService.getSuccessResponse(guestBookRegistResponseDto);
+        } catch (BaseException e) {
+            return baseResponseService.getFailureResponse(e.status);
+        }
+    }
 }
