ae561981de7541ce5a0a7afa36c6ab0f3a88293a
park98sj1@hanmail.net <park98sj1@hanmail.net>
2023-11-03 14:49:49 +0900

    Feat: 방명록 목록 조회 구현
    Former-commit-id: fe222c1e8d84f5a42b82a250ab447b4968dbf51c

--- backend/src/main/java/com/ssafy/moeutto/domain/guestBook/controller/GuestBookController.java
+++ backend/src/main/java/com/ssafy/moeutto/domain/guestBook/controller/GuestBookController.java
@@ -1,6 +1,7 @@
 package com.ssafy.moeutto.domain.guestBook.controller;
 
 import com.ssafy.moeutto.domain.guestBook.dto.request.GuestBookRegistRequestDto;
+import com.ssafy.moeutto.domain.guestBook.dto.response.GuestBookListResponseDto;
 import com.ssafy.moeutto.domain.guestBook.dto.response.GuestBookRegistResponseDto;
 import com.ssafy.moeutto.domain.guestBook.service.GuestBookService;
 import com.ssafy.moeutto.domain.member.auth.AuthTokensGenerator;
@@ -11,6 +12,7 @@ import com.ssafy.moeutto.global.response.BaseResponseStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -43,6 +45,30 @@ public class GuestBookController {
             // 방명록 등록
             GuestBookRegistResponseDto guestBookRegistResponseDto = guestBookService.registGuestBook(memberId, guestBookRegistRequestDto);
             return baseResponseService.getSuccessResponse(guestBookRegistResponseDto);
+        } catch (BaseException e) {
+            return baseResponseService.getFailureResponse(e.status);
+        }
+    }
+
+    /**
+     * 옷장을 조회할 때, 사용자 id에 따른 방명록 목록을 반환합니다.
+     *
+     * @param token
+     * @return List<GuestBookListResponseDto>
+     */
+    @GetMapping("")
+    public BaseResponse<Object> listGuestBook(@RequestHeader(value = "accessToken", required = false) String token) {
+        try {
+            // 토큰 정보 체크
+            if (token == null || token.equals("")) {
+                throw new BaseException(BaseResponseStatus.SESSION_EXPIRATION);
+            }
+
+            UUID memberId = authTokensGenerator.extractMemberId(token); // 사용자 체크
+
+            // 방명록 등록
+            List<GuestBookListResponseDto> guestBookListResponseDtoList = guestBookService.listGuestBook(memberId);
+            return baseResponseService.getSuccessResponse(guestBookListResponseDtoList);
         } catch (BaseException e) {
             return baseResponseService.getFailureResponse(e.status);
         }
