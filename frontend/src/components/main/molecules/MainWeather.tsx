bde706d179e2f28154e48a99642222170c314ab3
seoyoung81 <seosue0801@gmail.com>
2023-11-06 00:39:03 +0900

    Design: 메인 페이지 모바일 UI CSS 수정
    Former-commit-id: e78b502cd21aa081a9a6530e2ef8e3b7dd374810

--- frontend/src/components/main/molecules/MainWeather.tsx
+++ frontend/src/components/main/molecules/MainWeather.tsx
@@ -1,18 +1,23 @@
 import TodayTemp from "../atoms/TodayTemp";
 import TodayDate from "../atoms/TodayDate";
+import TodayTip from "../atoms/TodayTip";
+
 
 const MainWeather = () => {
     return (
         <>
-            <div className="my-4 bg-[#FFD8E5] rounded-2xl shadow-md">
-                <img src="/images/weather1.png" alt="" />
+        <div className="relative bg-[#FFD8E5] rounded-2xl shadow-md my-4">
+            <div className="p-4 mt-16 flex flex-col justify-end items-end">
                 <TodayDate />
                 <TodayTemp />
-                <div className="text-[#FF7C7C] text-AppBody1">오늘은 날씨가 흐리고 <br/> 바람이 많이 붑니다</div>
             </div>
-            <div className="bg-gray-button rounded-xl shadow-md p-2">
-                <div className="text-AppBody2 font-bold">TIP. 보온에 신경쓰세요!</div>
-                <div className="text-AppBody2 font-bold">TIP. 저녁에 추워요. 아우터를 챙기세요!</div>
+            <div className="text-[#FF7C7C] text-AppBody1 p-4">오늘은 날씨가 흐리고 <br/> 바람이 많이 붑니다</div>
+            <img src="/images/weather1.png" className="w-1/2 absolute -top-20"/>
+        </div>
+            <div className="bg-gray-button rounded-xl shadow-md p-3 relative">
+                <img src="/images/light.png" className="w-[10%] absolute -top-6 right-4" />
+                <TodayTip content="TIP. 보온에 신경쓰세요!" />
+                <TodayTip content="TIP. 저녁에 추워요. 아우터를 챙기세요!" />
             </div>
         </>
     )
