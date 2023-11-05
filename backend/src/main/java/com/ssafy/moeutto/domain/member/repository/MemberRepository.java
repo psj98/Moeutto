9a4c3f7cdd016113def0e151e6380ae7ca9adaa8
27kanghan <27kanghan@naver.com>
2023-11-05 23:57:27 +0900

    Fix:쿼리문 오류 수정
    Former-commit-id: 84c3b46777216943818be375258ca85e2acdf486

--- backend/src/main/java/com/ssafy/moeutto/domain/member/repository/MemberRepository.java
+++ backend/src/main/java/com/ssafy/moeutto/domain/member/repository/MemberRepository.java
@@ -22,16 +22,22 @@ public interface MemberRepository extends JpaRepository<Member, UUID> {
     Member findMemberById(UUID memberId);
 
 
+    /**
+     * 닉네임과 내 아이디를 기반으로 친구 목록 검색.
+     * 팔로우 여부 위해 my_id(memberId)필요
+     * @param memberId
+     * @param nickname
+     * @return
+     */
 
-
-    @Query(value = "SELECT m.profile_image as profileImageUrl," +
-            "m.nickname as nickname," +
-            "m.emaiol as email," +
-            "EXSISTS (" +
+    @Query(value = "SELECT m.profile_image as profileImageUrl, " +
+            "m.nickname as nickname, " +
+            "m.email as email, " +
+            "EXISTS (" +
             "SELECT 1 FROM following f1 " +
-            "WHERE f1.my_id = ?1 AND f1.following_id = m.id as isFollwing) " +
+            "WHERE f1.my_id = ?1 AND f1.following_id = m.id) as isFollowing " +
             "FROM member m " +
-            "WHERE m.nickname LIKE CONCAT('%', ninkname = ?2, '%')", nativeQuery = true)
+            "WHERE m.nickname LIKE CONCAT('%', ?2, '%')", nativeQuery = true)
     List<FriendsListResponseDto> findFriendsListByNickname(UUID memberId, String nickname);
 
 
