d1b2103bdd8167dafe35fdd3ae55de0fa06924d5
seoyoung81 <seosue0801@gmail.com>
2023-11-06 00:39:03 +0900

    Design: 메인 페이지 모바일 UI CSS 수정
    Former-commit-id: 7f4ae59caf39a2638d67c697ffe932f825d0fa3e

--- frontend/src/components/common/Alert.tsx
+++ frontend/src/components/common/Alert.tsx
@@ -2,7 +2,7 @@ const Alert = () => {
     return (
         <>
         {/* 반응형 어떻게? */}
-            <img src="/images/alert.png" alt="alert" className="w-1/6 absolute right-0 mt-[3%]" />
+            <img src="/images/alert.png" alt="alert" className="w-[10%] fixed mt-[3%]" />
         </>
     )
 
--- frontend/src/components/main/atoms/TodayTemp.tsx
+++ frontend/src/components/main/atoms/TodayTemp.tsx
@@ -2,8 +2,8 @@ const TodayTemp = () => {
     return (
         <>
         <div className="flex text-AppBody1">
-            <div className="text-[#0D009E]">8℃</div>
-            <div className="text-[#EE002B]">17℃</div>
+            <div className="text-[#0D009E] tracking-wider">8℃</div>
+            <div className="text-[#EE002B] tracking-wider">17℃</div>
         </div>
         </>
     )
