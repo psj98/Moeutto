573f25c239d463c81a07fe332c3e71ad4edefc88
seoyoung81 <seosue0801@gmail.com>
2023-11-06 10:53:21 +0900

    Design: alert 컴포넌트 css 수정
    Former-commit-id: 27372ad7b12f6f27b29a5eea3158e6df5deec31a

--- frontend/src/components/common/Alert.tsx
+++ frontend/src/components/common/Alert.tsx
@@ -2,7 +2,7 @@ const Alert = () => {
     return (
         <>
         {/* 반응형 어떻게? */}
-            <img src="/images/alert.png" alt="alert" className="w-[5%] min-w-[70px] fixed mt-[3%] right-0" />
+            <img src="/images/alert.png" alt="alert" className="w-[5%] min-w-[70px] absolute mt-[3%] right-0" />
         </>
     )
 
--- frontend/src/pages/MyClosetPage.tsx
+++ frontend/src/pages/MyClosetPage.tsx
@@ -114,7 +114,7 @@ const MyClosetPage = () => {
 
   useEffect(() => {
     fetchData();
-  }, [categoryId, sortBy, orderBy]);
+  }, [categoryId]);
 
   return (
     <>
