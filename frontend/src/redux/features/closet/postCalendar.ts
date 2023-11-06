01fb1c67bf113e0df55048ddc0464dfc0fe55144
s01k1m <sk618dev@gmail.com>
2023-11-06 16:57:25 +0900

    Feat: redux state로 이미지 url 저장 구현
    Former-commit-id: adededdfba0f72ea5284d2fbf743c85b3ed4b006

---
+++ frontend/src/redux/features/closet/postCalendar.ts
@@ -0,0 +1,38 @@
+// selectCloset 관련 redux toolkit 모듈 파일
+import { createSlice } from '@reduxjs/toolkit';
+
+// 타입
+interface SelectedClosetComponentState {
+  selectedClosetUrls: string[];
+}
+
+// 초기 상태 정의
+const initialState: SelectedClosetComponentState = {
+  // 빈 리스트
+  selectedClosetUrls: [],
+};
+
+// 액션 생성 함수
+const postCalendar = createSlice({
+  // 액션 타입 정의
+  name: 'postCalendar',
+  // 초기 상태
+  initialState,
+  // 리듀서 맵
+  reducers: {
+    // 리듀서 함수
+    // 아래 코드는 리듀서와 액션 생성자 함수가 분리되지 않은 형태로 작성함
+    selectItem: (state, action) => {
+      if (action.payload) {
+        // 이미지 url이 들어오면 리덕스가 관리하는 state에 추가합니다
+        state.selectedClosetUrls = [...state.selectedClosetUrls, action.payload];
+      }
+      console.log(action.payload);
+      console.log(state.selectedClosetUrls);
+    },
+  },
+});
+
+export const { selectItem } = postCalendar.actions;
+
+export default postCalendar.reducer;
--- frontend/src/redux/rootReducer.ts
+++ frontend/src/redux/rootReducer.ts
@@ -1,11 +1,11 @@
 // rootReducer.js
-import { combineReducers } from "redux";
-import selectClosetReducer from "./features/closet/selectClosetSlice";
-
+import { combineReducers } from 'redux';
+import selectClosetReducer from './features/closet/selectClosetSlice';
+import postCalendar from './features/closet/postCalendar';
 
 const rootReducer = combineReducers({
   closet: selectClosetReducer,
-  // 추가적인 리듀서가 있다면 여기에 추가
+  postCalendar,
 });
 
 export default rootReducer;
