956c08fea3f9a24e49dad4e066aa8c0c02822651
hyeonwoo <hjk8596@naver.com>
2023-11-06 16:33:12 +0900

    Fix: api.ts, application-prod.properties
    Former-commit-id: 994ee136433239143be75eae2b0f28ff422c28e6

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -4,11 +4,11 @@ import axios, { AxiosInstance } from 'axios';
 //   'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhM2FiM2ExNS00M2I0LTQ4OTYtYjFlMi0wM2Q2YWJhNWM2NmMiLCJleHAiOjE2OTg5NzM5NjJ9.vZxmr2SzJHm0nfq6_0qwgu_fsmtgZHxJfDf6CfZBCQmTQejYA9mWDvOc7TRXx7oPbwcHC9zrkF1gAj-aGrIqmw';
 
 // window.sessionStorage.setItem('accessToken', token);
-
+const apiURL = process.env.REACT_APP_API;
 // 인증 값이 필요 없는 경우
 const axiosApi = () => {
   const instance: AxiosInstance = axios.create({
-    baseURL: 'http://localhost:8080/api',
+    baseURL: `${apiURL}/api`,
   });
 
   return instance;
@@ -28,7 +28,7 @@ const axiosWithAuth = ({ ContentType }: { ContentType: string }): AxiosInstance 
   }
 
   const instance: AxiosInstance = axios.create({
-    baseURL: 'http://localhost:8080/api', // 여기에 기본 API URL을 넣어주세요.
+    baseURL: `${apiURL}/api`, // 여기에 기본 API URL을 넣어주세요.
     headers,
   });
 
