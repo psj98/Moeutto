30edd23953ef4df728bfa1b2b3bbd51bd6baeeb5
27kanghan <27kanghan@naver.com>
2023-11-06 16:45:38 +0900

    Feat: 로그인 CSS 구현
    Former-commit-id: 3e33254abf4d7a02f9e9a31851810a3a997b74a3

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,31 +1,43 @@
-// import { defaultInstance } from '../api/api';
-// import { useNavigate } from 'react-router-dom';
-// import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import kakaoButton from '../assets/images/kakao_login_button.png';
-
-export interface Login {
-  restApiKey: String;
-  redirectUri: String;
-  link: String;
-}
+import closetImage from '../assets/images/closet_image.png';
 
 const LoginPage = () => {
+  // 환경변수에서 redirect URI와 Kakao Client ID를 불러옵니다.
   const redirectUrl = process.env.REACT_APP_REDIRECT_URL;
-  // const navigate = useNavigate();
-
-  //   const axiosInstance = defaultInstance();
-
-  //   const redirectUrl =
-  // const navigate = useNavigate();
-  const link = `https://kauth.kakao.com/oauth/authorize?client_id=bab90d2b24304bb1f5b4c07938ff0fcc&redirect_uri=${redirectUrl}&response_type=code`;
+  const kakaoClientId = process.env.REACT_APP_KAKAO_CLIENT_ID;
+  const link = `https://kauth.kakao.com/oauth/authorize?client_id=${kakaoClientId}&redirect_uri=${redirectUrl}&response_type=code`;
 
   const loginHandler = () => {
     window.location.href = link;
   };
 
+  // useEffect를 사용하여 컴포넌트가 마운트 될 때 그라데이션 배경을 적용합니다.
+  useEffect(() => {
+    const body = document.body;
+
+    body.style.background = 'linear-gradient(to bottom, #FEDFEA 0%, #FFFFFF 100%)';
+
+    return () => {
+      body.style.background = '';
+    };
+  }, []);
+
   return (
-    <div>
-      <img src={kakaoButton} className="" onClick={loginHandler}></img>
+    <div className="flex flex-col items-center justify-center min-h-screen ">
+      <div className="mb-4 text-center text-AppBody1 font-black title">
+        <div>당신의</div>
+        <div>옷장을</div>
+        <div>스마트하게</div>
+      </div>
+      <img src={closetImage}></img>
+      <button onClick={loginHandler} className="focus:outline-none">
+        <img
+          src={kakaoButton}
+          className="cursor-pointer transform transition duration-200 hover:scale-105"
+          alt="카카오 로그인 버튼"
+        />
+      </button>
     </div>
   );
 };
