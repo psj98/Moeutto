de2b2e4777b70efe4f1e325fed44ed4ffd0de2be
seoyoung81 <seosue0801@gmail.com>
2023-11-06 18:11:55 +0900

    Feat: 나의 옷장 페이지 UI 재구성
    전체 버튼이 여러개여서 클릭이 안되는 이슈 있음
    
    
    Former-commit-id: 2ab9c1543a7d0adb666db1e46af83abe0ff29e5b

---
+++ frontend/src/components/mycloset/organisms/CategoryTapOrganism.tsx
@@ -0,0 +1,14 @@
+import CategoryTap from "../molecules/CategoryTap";
+
+const CategoryTapOrganism = () => {
+    return (
+        <>
+            <CategoryTap title="상의" id="002000" categories={['전체', '반팔', '후드', '맨투맨']} />
+            <CategoryTap title="하의" id="003000" categories={['전체', '청바지', '반바지', '카고팬츠']} />
+            <CategoryTap title="아우터" id="001000" categories={['전체', '패딩', '자켓', '코트']} />
+            <CategoryTap title="아이템" id="004000" categories={['전체', '귀마개', '장갑', '목도리']} />
+        </>
+    )
+}
+
+export default CategoryTapOrganism;
