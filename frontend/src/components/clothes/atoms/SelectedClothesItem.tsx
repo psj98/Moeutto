2545cb6382fd1c20c5b008f12b0754640d4ec8e0
s01k1m <sk618dev@gmail.com>
2023-11-06 16:56:56 +0900

    Design: 폰트 크기 축소와 fixed 스크롤 및 버튼 위치 조정
    Former-commit-id: 56aba2a1cdfc48a82c1b4b0aa36440f626a911b3

--- frontend/src/components/clothes/atoms/SelectedClothesItem.tsx
+++ frontend/src/components/clothes/atoms/SelectedClothesItem.tsx
@@ -1,35 +1,45 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { selectCloset } from "../../../redux/features/closet/selectClosetSlice";
-
+import { useLocation } from 'react-router-dom';
+import { selectCloset } from '../../../redux/features/closet/selectClosetSlice';
+import { selectItem } from '../../../redux/features/closet/postCalendar';
 
 interface ItemPropsType {
-    imgUrl: string;
-    clothesId: string;
+  imgUrl: string;
+  clothesId: string;
 }
 
 const SelectedClothesItem: React.FC<ItemPropsType> = ({ imgUrl, clothesId }) => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  const { pathname } = useLocation();
 
-    const [isSelected, setIsSelected] = useState(false);
+  console.log(pathname);
 
-    const handleClick = (e) => {
-        setIsSelected(!isSelected);
-        dispatch(selectCloset(e.target.id));
-        console.log(e.target.id);
-    };
+  const [isSelected, setIsSelected] = useState(false);
+  // pickpick 페이지에서 사용되는 함수입니다
+  const handleClick = e => {
+    setIsSelected(!isSelected);
+    dispatch(selectCloset(e.target.id));
+    console.log(e.target.id);
+  };
+  // calendar post 페이지에서 사용되는 함수입니다
+  const handlePostCalendar = e => {
+    dispatch(selectItem(imgUrl)); // 이미지 url을 redux state array 로 저장합니다
+  };
 
-    return (
-        <>
-            <img 
-                src={imgUrl} 
-                id={clothesId}
-                alt="옷" 
-                className={`w-[13vw] h-[13vw] border border-gray rounded-3xl ${isSelected ? 'bg-gray-300 border-pink-hot' : ''}`}
-                onClick={handleClick}
-            />
-        </>
-    );
-}
+  return (
+    <>
+      <img
+        src={imgUrl}
+        id={clothesId}
+        alt="옷"
+        className={`w-[13vw] h-[13vw] border border-gray rounded-3xl ${
+          isSelected ? 'bg-gray-300 border-pink-hot' : ''
+        }`}
+        onClick={pathname === '/pickpick' ? handleClick : handlePostCalendar}
+      />
+    </>
+  );
+};
 
 export default SelectedClothesItem;
