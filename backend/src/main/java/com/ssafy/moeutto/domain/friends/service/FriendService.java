3a01959de16b617d30a093261a732cae29ae2d50
27kanghan <27kanghan@naver.com>
2023-11-06 10:41:51 +0900

    Test: Friends API 테스트 완료
    Former-commit-id: 8490e34ee13e339e441e43a5fdd2174da4a7376a

--- backend/src/main/java/com/ssafy/moeutto/domain/friends/service/FriendService.java
+++ backend/src/main/java/com/ssafy/moeutto/domain/friends/service/FriendService.java
@@ -2,8 +2,8 @@ package com.ssafy.moeutto.domain.friends.service;
 
 import com.ssafy.moeutto.domain.friends.dto.request.FollowRequestDto;
 import com.ssafy.moeutto.domain.friends.dto.request.FriendsListRequestDto;
-import com.ssafy.moeutto.domain.friends.dto.response.FriendsListResponseDto;
-import com.ssafy.moeutto.domain.friends.dto.response.MyFriendsListResponseDto;
+import com.ssafy.moeutto.domain.friends.dto.response.IFriendsListResponseDto;
+import com.ssafy.moeutto.domain.friends.dto.response.IMyFriendsListResponseDto;
 import com.ssafy.moeutto.global.response.BaseException;
 
 import java.util.List;
@@ -11,7 +11,7 @@ import java.util.UUID;
 
 public interface FriendService {
     void follow(UUID memberId, FollowRequestDto requestDto) throws BaseException;
-    List<FriendsListResponseDto> searchFriends(UUID memberId, FriendsListRequestDto requestDto) throws BaseException;
+    List<IFriendsListResponseDto> searchFriends(UUID memberId, FriendsListRequestDto requestDto) throws BaseException;
 
-    List<MyFriendsListResponseDto> searchMyFollowinglist(UUID memberId) throws BaseException;
+    List<IMyFriendsListResponseDto> searchMyFollowinglist(UUID memberId) throws BaseException;
 }
