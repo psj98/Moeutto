c0e89263cedec52e84da1336f8cebe043fe3aa41
seoyoung81 <seosue0801@gmail.com>
2023-11-07 10:55:19 +0900

    Desgin: 책갈피 CSS
    Former-commit-id: 4ca6b10cf1d3930df182d33907cc8dc2191c5834

--- frontend/src/components/clothes/atoms/ClothesDetailImg.tsx
+++ frontend/src/components/clothes/atoms/ClothesDetailImg.tsx
@@ -7,26 +7,61 @@ interface PropsType {
     setStar: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
+// 그냥 여기서 즐겨찾기 api 불러오기
+
 const ClothesDetailImg = ({ imgUrl, star, setStar }: PropsType ) => {
     const onClickBookMark = () => {
         setStar((prev) => !prev)
     }
 
+    // let width: number = 15;
+    // let c: string = "";
+
+    // useEffect(() => {
+    //     const setRightStyle = () => {
+    //         const element = document.querySelector<HTMLElement>(".babo");
+    
+    //         if (element&& window.screen.availWidth > 390) {
+    //             const width = Math.min(document.getElementById("img").clientWidth * 0.3, 50);
+    //             const c = `${width.toString()}px`;
+    
+    //             console.log(c, typeof c, "11111");
+    
+    //             element.style.right = c;
+    //         }else{
+    //             const c = `20px`;
+    
+    //             console.log(c, typeof c, "11111");
+    
+    //             element.style.right = c;
+    //         }
+    //     };
+    
+    //     // Run the function after the initial render to ensure that the DOM is ready.
+    //     setTimeout(setRightStyle, 0);
+    // }, []);
+    
+
     return (
         <>
-            <div className="flex flex-col items-center justify-center relative">
-                <img 
-                    src={imgUrl} alt="clothes" 
-                    className="w-2/3 h-2/3 min-w-[223px] border rounded-3xl " 
-                />
-                <div className="absolute -top-3 end-9 sm:right-4">
-                    {star ? (
-                        <RxBookmark size={90} color="FAA0BF" onClick={onClickBookMark} />
-                    ) : (
-                        <RxBookmarkFilled size={90} color="FAA0BF" onClick={onClickBookMark} />
-                    )}
+            <div className="w-full baboParent flex flex-col items-center relative">
+                <div className="relative">
+                    <img 
+                        id="img"
+                        src={imgUrl} alt="clothes" 
+                        className="w-2/3 h-2/3 min-w-[223px] m-auto border rounded-3xl border" 
+                    />
+                    <div className={`babo absolute -top-2 w-[60px] right-[20px] sm:right-[30px] md:right-[50px] lg:right-[60px]`}>
+                        {star ? (
+                                <RxBookmark size={60} color="FAA0BF" onClick={onClickBookMark} />
+                            
+                            ) : (
+                                <RxBookmarkFilled size={60} color="FAA0BF" onClick={onClickBookMark} />
+                                )}
+                    </div>
+
                 </div>
-            </div>
+                </div>
         </>
     )
 }
