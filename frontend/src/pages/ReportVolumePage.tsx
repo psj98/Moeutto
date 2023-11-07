3f9eb5945dd9e321b65578bbc149ecf8c060bfc1
hyeonwoo <hjk8596@naver.com>
2023-11-07 17:51:30 +0900

    same
    Former-commit-id: 65c21b2efe17291ca0d9185612b95a6617cefb03

--- frontend/src/pages/ReportVolumePage.tsx
+++ frontend/src/pages/ReportVolumePage.tsx
@@ -1,12 +1,40 @@
+import { useEffect, useState } from 'react';
 import IntroComment from '../components/report/atoms/IntroComment';
 import ShortReportComment from '../components/report/atoms/ShortReportComment';
 import ReportComment from '../components/report/atoms/ReportComment';
 import ReportAvg from '../components/report/atoms/ReportAvg';
+import { authInstance } from '../api/api';
 
 const ReportVolumePage = () => {
+  const [myTotalAmount, setMyTotalAmount] = useState('');
+  // const [myAnalysisAmount, setMyAnalysisAmount] = useState([]);
+  const [userTotalAmountAvg, setUserTotalAmountAvg] = useState('');
+  const [shortReportComment, setShortReportComment] = useState('');
+  const [reportComment, setReportComment] = useState('');
+
+  const fetchData = async () => {
+    const axiosInstance = authInstance({ ContentType: 'application/json' });
+    const response = await axiosInstance.get('/clothes/analysis-amount');
+
+    console.log('지금 데이터 값은?', response);
+    console.log('myTotalAmount : ', response.data.data.myTotalAmount);
+    console.log('myAnalysisAmount : ', response.data.data.myAnalysisAmount);
+
+    setMyTotalAmount(response.data.data.myTotalAmount);
+    setUserTotalAmountAvg(response.data.data.userTotalAmountAvg);
+    // setMyAnalysisAmount(response.data.data.myAnalysisAmount);
+    // 아래 삼항연산자는 추후 값을 조정해서
+    setShortReportComment(myTotalAmount >= userTotalAmountAvg ? '집이 넓으신가요?' : '집에 도둑이 들렀나요?');
+    setReportComment(myTotalAmount >= userTotalAmountAvg ? '당신은 맥시멀리스트입니다!' : '당신은 미니멀리스트입니다!');
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <>
-      {/* 인트로 분석 문구 */}
+      {/* 인트로 분석 문구 , 닉네임 받아야함*/}
       <IntroComment nickname="모드리치" imageUrl="/images/report.png" />
 
       {/* 간단 분석 문구 */}
@@ -14,8 +42,7 @@ const ReportVolumePage = () => {
         imageDivClass="absolute top-[-45px] left-[0px]"
         imageUrl="/images/box.png"
         imageClass="w-24 inline-block"
-        mainTitle="집이 넓어요?"
-        subTitle="6개월을 기준으로 계산해봤어요"
+        mainTitle={`${shortReportComment}`}
       />
 
       {/* 분석 문구 */}
@@ -23,7 +50,7 @@ const ReportVolumePage = () => {
         imageUrl="/images/report-closet.png"
         imageClass="w-16 inline-block"
         mainTitle="내 옷장에는 몇 벌의 옷이 있을까요?"
-        subTitle={`당신은 엄청난 맥시멀리스트입니다!`}
+        subTitle={`${reportComment}`}
       />
 
       {/* 옷 개수 */}
@@ -31,9 +58,9 @@ const ReportVolumePage = () => {
         imageUrl="/images/hanger.png"
         imageAlt="옷걸이 이미지"
         myClosetTitle="나의 옷장"
-        myClosetValue="48벌"
+        myClosetValue={`${myTotalAmount}벌`}
         avgClosetTitle="모으또 옷장 평균"
-        avgClosetValue="56벌"
+        avgClosetValue={`${userTotalAmountAvg}벌`}
       />
     </>
   );
