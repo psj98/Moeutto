48d86ea8278d03df7a64d72774baa5660a83a3fb
park98sj1@hanmail.net <park98sj1@hanmail.net>
2023-11-07 16:53:25 +0900

    Feat: 옷 빈도 분석 CSS 구현
    Former-commit-id: 0e49d463ae96f6ac1c162bdb4cbe3297839e7f0a

--- frontend/src/components/report/atoms/ReportAvg.tsx
+++ frontend/src/components/report/atoms/ReportAvg.tsx
@@ -1,37 +1,27 @@
 import ReportAvgCard from './ReportAvgCard';
 
 interface ReportAvgProps {
-  imageUrl: string;
-  imageAlt: string;
-  myClosetTitle: string;
-  myClosetValue: string;
-  avgClosetTitle: string;
-  avgClosetValue: string;
+  image: imageObject;
+  myCloset: closetObject;
+  avgCloset: closetObject;
 }
 
-const ReportAvg = ({
-  imageUrl,
-  imageAlt,
-  myClosetTitle,
-  myClosetValue,
-  avgClosetTitle,
-  avgClosetValue,
-}: ReportAvgProps) => {
+interface imageObject {
+  url: string;
+  alt: string;
+}
+
+interface closetObject {
+  title: string;
+  value: string;
+}
+
+const ReportAvg = ({ image, myCloset, avgCloset }: ReportAvgProps) => {
   return (
     <>
       <div className="flex flex-row justify-evenly mb-10">
-        <ReportAvgCard
-          imageUrl={imageUrl}
-          imageAlt={imageAlt}
-          closetTitle={myClosetTitle}
-          closetValue={myClosetValue}
-        />
-        <ReportAvgCard
-          imageUrl={imageUrl}
-          imageAlt={imageAlt}
-          closetTitle={avgClosetTitle}
-          closetValue={avgClosetValue}
-        />
+        <ReportAvgCard image={image} closet={myCloset} />
+        <ReportAvgCard image={image} closet={avgCloset} />
       </div>
     </>
   );
