fe53843cad9bc9380e182ddffa1c5ae57f6903c4
27kanghan <27kanghan@naver.com>
2023-11-08 14:42:44 +0900

    Design: login bottom image
    Former-commit-id: 5d782d938c4e23ddefa34539d0a47ea48693bb2d

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 // import { useNavigate } from 'react-router-dom';
 import kakaoButton from '../assets/images/kakao_login_button.png';
 import closetImage from '../assets/images/closet_image.png';
+import loginBottom from '../assets/images/login_bottom.png';
 
 const LoginPage = () => {
   // 환경변수에서 redirect URI와 Kakao Client ID를 불러옵니다.
@@ -30,27 +31,30 @@ const LoginPage = () => {
   }, []);
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen ">
-      <div className="mb-4 text-center text-AppBody1 font-black title">
-        <div>당신의</div>
-        <div>옷장을</div>
-        <div>스마트하게</div>
+    <div className="login-container">
+      <div className="flex flex-col items-center justify-center h-[590px] ">
+        <div className="mb-4 text-center text-AppBody1 font-black title">
+          <div>당신의</div>
+          <div>옷장을</div>
+          <div>스마트하게</div>
+        </div>
+        <img src={closetImage}></img>
+        <button
+          onClick={() =>
+            // handleNavigate(
+            //   `https://kauth.kakao.com/oauth/authorize?client_id=bab90d2b24304bb1f5b4c07938ff0fcc&redirect_uri=${redirectUrl}&response_type=code`
+            // )
+            loginHandler()
+          }
+          className="focus:outline-none">
+          <img
+            src={kakaoButton}
+            className="cursor-pointer transform transition duration-200 hover:scale-105"
+            alt="카카오 로그인 버튼"
+          />
+        </button>
       </div>
-      <img src={closetImage}></img>
-      <button
-        onClick={() =>
-          // handleNavigate(
-          //   `https://kauth.kakao.com/oauth/authorize?client_id=bab90d2b24304bb1f5b4c07938ff0fcc&redirect_uri=${redirectUrl}&response_type=code`
-          // )
-          loginHandler()
-        }
-        className="focus:outline-none">
-        <img
-          src={kakaoButton}
-          className="cursor-pointer transform transition duration-200 hover:scale-105"
-          alt="카카오 로그인 버튼"
-        />
-      </button>
+      <img src={loginBottom}></img>
     </div>
   );
 };
