69ece20ba564657e8761d462d37c6013db89725c
27kanghan <27kanghan@naver.com>
2023-11-08 13:45:04 +0900

    Design: TutorialPage Css
    Former-commit-id: 26846d043e725981680dbddcd0179d4fc4133120

--- frontend/src/pages/TutorialPage.tsx
+++ frontend/src/pages/TutorialPage.tsx
@@ -3,7 +3,7 @@
 import { Pagination, Navigation, Autoplay } from 'swiper/modules';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
-
+import { useNavigate } from 'react-router-dom';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/autoplay';
@@ -11,45 +11,60 @@ import 'swiper/css/autoplay';
 import tutorialImageFirst from '../assets/images/tutoral_1.png';
 
 const TutorialPage = () => {
+  const naviagte = useNavigate();
+
+  const goMainPage = () => {
+    naviagte('/main');
+  };
+
   return (
-    <div className="swiper-container">
+    <div className="h-[full]]">
       <div className="title flex justify-center">튜토리얼</div>
-      <Swiper
-        modules={[Pagination, Navigation, Autoplay]}
-        spaceBetween={50}
-        navigation
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
-        pagination={{ clickable: true }}
-        className={`
-       `}>
-        <SwiperSlide>
-          <div>
-            <img src={tutorialImageFirst} className="w-full h-auto object-cover"></img>
-          </div>
-        </SwiperSlide>
+      <div className="h-[500px]">
+        <Swiper
+          className="mt-[125px] mb-[90px] "
+          modules={[Pagination, Navigation, Autoplay]}
+          spaceBetween={100}
+          navigation
+          autoplay={{
+            delay: 5500,
+            disableOnInteraction: false,
+          }}
+          pagination={{ clickable: true }}
+          onSwiper={swiper => console.log(swiper)}
+          onSlideChange={() => console.log('slide change')}>
+          <SwiperSlide>
+            <div className="">
+              <img src={tutorialImageFirst} className="w-full h-auto object-cover"></img>
+            </div>
+          </SwiperSlide>
 
-        <SwiperSlide>
-          <div>
-            <img src={tutorialImageFirst} className="w-full h-auto object-cover"></img>
-          </div>
-        </SwiperSlide>
+          <SwiperSlide>
+            <div>
+              <img src={tutorialImageFirst} className="w-full h-auto object-cover"></img>
+            </div>
+          </SwiperSlide>
 
-        <SwiperSlide>
-          <div>
-            <img src={tutorialImageFirst} className="w-full h-auto object-cover"></img>
-          </div>
-        </SwiperSlide>
+          <SwiperSlide>
+            <div>
+              <img src={tutorialImageFirst} className="w-full h-auto object-cover"></img>
+            </div>
+          </SwiperSlide>
 
-        <SwiperSlide>
-          <div>
-            <img src={tutorialImageFirst} className="w-full h-auto object-cover"></img>
-          </div>
-        </SwiperSlide>
-      </Swiper>
-      <div></div>
+          <SwiperSlide>
+            <div>
+              <img src={tutorialImageFirst} className="w-full h-auto object-cover"></img>
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </div>
+      <div className="flex justify-center ">
+        <button
+          onClick={goMainPage}
+          className="bg-pink-hot rounded-xl shadow-xl text-white text-WebBody3 p-4 tracking-wider font-bold">
+          건너뛰기
+        </button>
+      </div>
     </div>
   );
 };
