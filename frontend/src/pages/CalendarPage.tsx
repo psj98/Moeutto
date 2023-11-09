a7447350489c02fed85e34349f2f4c8bae402bc1
seoyoung81 <seosue0801@gmail.com>
2023-11-09 14:31:46 +0900

    Feat: 캘린더 코드 복구
    Former-commit-id: f3310bd1064a33f803a0fd81736c68511e06bc44

--- frontend/src/pages/CalendarPage.tsx
+++ frontend/src/pages/CalendarPage.tsx
@@ -1,10 +1,17 @@
 import { Link } from 'react-router-dom';
+import CalendarTemplates from '../components/calendar/templates/CalendarTemplates';
 
 const CalendarPage = () => {
   return (
-    <button>
-      <Link to="post">포스트</Link>
-    </button>
+    <>
+      <button>
+        <Link to="post">포스트</Link>
+        <br />
+      </button>
+      <div className='flex justify-center items-center border rounded-2xl shadow-md border-pink border-2 p-4 shadow-md relative'>
+        <CalendarTemplates />
+      </div>
+    </>
   );
 };
 
