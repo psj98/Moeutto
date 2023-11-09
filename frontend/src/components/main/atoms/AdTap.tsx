01f098599e519283929a58698f5ce2d64e5852fd
hyeonwoo <hjk8596@naver.com>
2023-11-09 09:19:23 +0900

    Refactor: nickname 상수 String으로 되어있던 것 sessionStorage.getItem
    Former-commit-id: 1e7097849b81727467f5fab14ca556590b7c542e

--- frontend/src/components/main/atoms/AdTap.tsx
+++ frontend/src/components/main/atoms/AdTap.tsx
@@ -1,16 +1,16 @@
-import MainComment from "./MainComment";
+import MainComment from './MainComment';
 
 const AddTap = () => {
-    const userName: string = "뫄뫄뫄";
+  const userName: string = `${sessionStorage.getItem('nickname')}`;
 
-    return (
-        <> 
-            <div className="bg-white rounded-2xl shadow-md p-4 mt-10"> 
-                <MainComment title={`${userName}님 \n이런 옷은 어떤가요?`} />
-                <img src="" alt="" />
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="bg-white rounded-2xl shadow-md p-4 mt-10">
+        <MainComment title={`${userName}님 \n이런 옷은 어떤가요?`} />
+        <img src="" alt="" />
+      </div>
+    </>
+  );
+};
 
 export default AddTap;
