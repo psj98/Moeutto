738a0601fff033fca43a51b9294d9482e6993ca6
seoyoung81 <seosue0801@gmail.com>
2023-11-09 22:59:44 +0900

    Feat: 착장 등록 상세 조회 기능만 구현
    Former-commit-id: 849881a346345792da84abbffbfedc8f0a62ddf7

--- frontend/src/components/calendar/templates/CalendarTemplates.tsx
+++ frontend/src/components/calendar/templates/CalendarTemplates.tsx
@@ -1,9 +1,17 @@
 import Calendar from "../organisms/Calendar";
 
-const CalendarTemplates = () => {
+const CalendarTemplates = ({
+    setShowSelectedImg,
+    setClothesId,
+    setIsLikedOutFit
+}) => {
     return (
         <div className="">
-            <Calendar />
+            <Calendar
+                setShowSelectedImg={setShowSelectedImg}
+                setClothesId={setClothesId}
+                setIsLikedOutFit={setIsLikedOutFit}
+            />
         </div>
     )
 }
