df96f1d2e8a41ce14ba158d1731a8a4677f52e6f
s01k1m <sk618dev@gmail.com>
2023-11-09 10:46:03 +0900

    Feat: 친구 컴포넌트 작업 중
    Former-commit-id: 50a299a5468041d4c61b0cf10ba5d7e3e1c535c9

--- frontend/src/components/friend/templates/FriendListTemplate.tsx
+++ frontend/src/components/friend/templates/FriendListTemplate.tsx
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { Dispatch, SetStateAction } from 'react';
+import { BiSearchAlt } from 'react-icons/bi';
 import ProfileCard from '../molecules/ProfileCard';
 import { FriendListType } from '../../../pages/FriendListPage';
+import './style.css';
 
-const FriendListTemplate = ({ friendList }: FriendListType) => {
+type FriendListTemplateProps = {
+  friendList: FriendListType['friendList'];
+  setValue: PropType; // PropType는 필요한 타입으로 대체해야 합니다.
+};
+
+const FriendListTemplate = ({ friendList, setValue }) => {
   return (
     <div>
-      <div>여기 검색박스 있어야함</div>
-      <input type="text" id="search" placeholder="친구를 검색해보세요" />
+      <div className="relative w-[80%] m-auto drop-shadow-md">
+        <BiSearchAlt className="absolute top-[5px] left-3 changeColor " size="30" />
+        <input
+          type="text"
+          id="search"
+          placeholder="친구를 검색해보세요"
+          className="border rounded-full w-full h-[40px]  py-1 ps-12 text-AppBody1 leading-[20px] align-middle"
+          onChange={setValue}
+        />
+      </div>
       <div>Frined Lsit</div>
       {friendList
         ? friendList.map(item => {
             return (
               <ProfileCard
+                key={item.email}
                 nickname={item.nickname}
                 email={item.email}
                 profileImage={item.profileImage}
