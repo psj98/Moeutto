ab9fc5c468e11c0ed6af061da1b00f629449e88b
hyeonwoo <hjk8596@naver.com>
2023-11-10 11:24:44 +0900

    same
    Former-commit-id: 83ca6352a1684851758b4c8c6286ce7d03f31fa1

--- frontend/src/pages/MyPage.tsx
+++ frontend/src/pages/MyPage.tsx
@@ -4,8 +4,6 @@ import { authInstance } from '../api/api';
 
 import Label from '../components/add/atoms/Label';
 
-const handleSubmit = () => {};
-
 const Button = styled.button``;
 const Input = styled.input``;
 
@@ -20,7 +18,9 @@ const MyPage = () => {
   const [nickname, setNickname] = useState<string>();
   const [accountFind, setAccountFind] = useState<boolean>();
   const [closetFind, setClosetFind] = useState<boolean>();
+  const [imageUrl, setImageUrl] = useState<string>();
   const [myPageInfo, setMyPageInfo] = useState<MyPageItemType | null>();
+  // const [payload, setPayload] = useState<JSON | null>(null);
 
   const handleNicknameChange = (e: ChangeEvent<HTMLInputElement>) => {
     if (e.target.value) {
@@ -31,21 +31,49 @@ const MyPage = () => {
     }
   };
 
-  const handleClosetChange = (e: ChangeEvent<HTMLInputElement>) => {
-    // if (e.target.value) {
-    //   setClosetFind(e.target);
-    // }
-    if (e.target.checked) {
-      setClosetFind(!e.target.checked);
+  // console.log('accountFind', accountFind, '*********************');
+  const handleModifySubmit = async () => {
+    const data: MyPageItemType = {
+      nickname,
+      imageUrl,
+      closetFind,
+      accountFind,
+    };
+
+    const memberUpdateMyInfoRequestDto = JSON.stringify(data);
+
+    const axiosInstance = authInstance({ ContentType: 'application/json' });
+    const response = await axiosInstance.put('/members/modify', memberUpdateMyInfoRequestDto);
+
+    if (response.data) {
+      alert('회원 정보 변경 성공');
+    } else {
+      alert('회원 정보 변경 실패');
     }
-    console.log('옷장 공개 여부 ? ', closetFind);
+  };
+
+  const handleClosetChange = (e: ChangeEvent<HTMLInputElement>) => {
+    console.log('옷장 공개 ? ', e.target.checked);
+    const flag = e.target.checked;
+
+    setClosetFind(flag);
   };
 
   const handleAccountChange = (e: ChangeEvent<HTMLInputElement>) => {
-    if (e.target.checked) {
-      setAccountFind(e.target.value);
-    }
+    console.log('handleAccountChange 함수 호출됨');
+    console.log('계정 공개 ? ', e.target.checked);
+    const flag = e.target.checked;
+
+    setAccountFind(flag);
   };
+
+  useEffect(() => {
+    console.log('바뀐 옷장 공개 여부 ? ', closetFind);
+  }, [closetFind]);
+
+  useEffect(() => {
+    console.log('바뀐 계정 공개 여부 ? ', accountFind);
+  }, [accountFind]);
 
   const fetchData = async () => {
     try {
@@ -55,19 +83,11 @@ const MyPage = () => {
 
       console.log('마이페이지 데이터 조회 성공', response.data.data);
 
-      // const myInfo: myPageItem = response.data.data.MemberMyPageResponseDto(row => ({
-      //   nickname: row.nickname,
-      //   imageUrl: row.imageUrl,
-      //   closetFind: row.closetFind,
-      //   accountFind: row.accountFind,
-      // }));
-
       setMyPageInfo(response.data.data);
+      setImageUrl(response.data.data.imageUrl);
       setNickname(response.data.data.nickname);
       setAccountFind(response.data.data.accountFind);
       setClosetFind(response.data.data.closetFind);
-
-      console.log(accountFind, closetFind);
 
       console.log(myPageInfo);
     } catch (error) {
@@ -81,74 +101,73 @@ const MyPage = () => {
     fetchData();
   }, []);
 
-  // useEffect(() => {
-  //   console.log('체크', myPageInfo);
-  // }, [myPageInfo]);
-
   return (
     <>
-      <div className="flex flex-col text-AppBody1">
-        {/* 프로필 사진 */}
-        <div className="my-10 text-center">
-          <img className="w-24 inline-block" src={`${myPageInfo?.imageUrl}`} alt="프로필 이미지" />
-        </div>
+      {closetFind !== undefined && accountFind !== undefined ? (
+        <div className="flex flex-col text-AppBody1">
+          {/* 프로필 사진 */}
+          <div className="my-10 text-center">
+            <img className="w-24 inline-block" src={`${myPageInfo?.imageUrl}`} alt="프로필 이미지" />
+          </div>
+          {/* 닉네임 */}
+          <div className="mb-6">
+            {/* 닉네임 글자 */}
+            <Label id="nickName" value="닉네임" />
+            {/* 닉네임 Input */}
+            <Input
+              className="w-full border border-1 rounded-xl indent-1 p-1 text-AppBody1"
+              placeholder="닉네임"
+              value={nickname}
+              onChange={handleNicknameChange}></Input>
+          </div>
 
-        {/* 닉네임 */}
-        <div className="mb-6">
-          {/* 닉네임 글자 */}
-          <Label id="nickName" value="닉네임" />
-          {/* 닉네임 Input */}
-          <Input
-            className="w-full border border-1 rounded-xl indent-1 p-1 text-AppBody1"
-            placeholder="닉네임"
-            value={nickname}
-            onChange={handleNicknameChange}></Input>
-        </div>
+          {/* 옷장 공개 여부 */}
+          <div className="mb-6">
+            {/* 옷장 공개 여부 글자 */}
+            <p className="mb-2">옷장 공개 여부</p>
+            {/* on/off 버튼 */}
+            <label className="relative inline-flex items-center cursor-pointer">
+              <input
+                type="checkbox"
+                defaultChecked={closetFind}
+                onChange={handleClosetChange}
+                className="sr-only peer"
+              />
+              <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-0 peer-focus:ring-pink dark:peer-focus:ring-pink rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-pink"></div>
+            </label>
+          </div>
 
-        {/* 옷장 공개 여부 */}
-        <div className="mb-6">
-          {/* 옷장 공개 여부 글자 */}
-          <p className="mb-2">옷장 공개 여부</p>
-          {/* on/off 버튼 */}
-          <label className="relative inline-flex items-center cursor-pointer">
-            <input
-              type="checkbox"
-              // checked={myPageInfo?.accountFind}
-              defaultChecked={closetFind}
-              onChange={handleClosetChange}
-              className="sr-only peer"
-            />
-            <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-0 peer-focus:ring-pink dark:peer-focus:ring-pink rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-pink"></div>
-          </label>
-        </div>
+          {/* 계정 공개 여부 */}
+          <div className="mb-6">
+            {/* 계정 공개 여부 글자 */}
+            <p className="mb-2">계정 공개 여부</p>
+            {/* on/off 버튼 */}
+            <label className="relative inline-flex items-center cursor-pointer">
+              <input
+                type="checkbox"
+                defaultChecked={accountFind}
+                onChange={handleAccountChange}
+                className="sr-only peer"
+              />
+              <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-0 peer-focus:ring-pink dark:peer-focus:ring-pink rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-pink"></div>
+            </label>
+          </div>
 
-        {/* 계정 공개 여부 */}
-        <div className="mb-6">
-          {/* 계정 공개 여부 글자 */}
-          <p className="mb-2">계정 공개 여부</p>
-          {/* on/off 버튼 */}
-          <label className="relative inline-flex items-center cursor-pointer">
-            <input type="checkbox" checked={myPageInfo?.closetFind} className="sr-only peer" />
-            <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-0 peer-focus:ring-pink dark:peer-focus:ring-pink rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-pink"></div>
-          </label>
+          {/* 정보 수정 / 회원 탈퇴 버튼 */}
+          <div className="flex flex-row mb-4">
+            {/* 정보 수정 버튼 */}
+            <Button
+              className="pt-2 pb-1 px-3 mr-4 rounded-xl bg-neutral-100 shadow-[1px_4px_4px_0px_rgba(0,0,0,0.15)]"
+              onClick={handleModifySubmit}>
+              정보수정
+            </Button>
+            {/* 회원 탈퇴 버튼 */}
+            <Button className="pt-2 pb-1 px-3 rounded-xl bg-neutral-100 shadow-[1px_4px_4px_0px_rgba(0,0,0,0.15)]">
+              회원탈퇴
+            </Button>
+          </div>
         </div>
-
-        {/* 정보 수정 / 회원 탈퇴 버튼 */}
-        <div className="flex flex-row mb-4">
-          {/* 정보 수정 버튼 */}
-          <Button
-            className="pt-2 pb-1 px-3 mr-4 rounded-xl bg-neutral-100 shadow-[1px_4px_4px_0px_rgba(0,0,0,0.15)]"
-            onChange={handleSubmit}>
-            정보수정
-          </Button>
-          {/* 회원 탈퇴 버튼 */}
-          <Button
-            className="pt-2 pb-1 px-3 rounded-xl bg-neutral-100 shadow-[1px_4px_4px_0px_rgba(0,0,0,0.15)]"
-            onChange={handleSubmit}>
-            회원탈퇴
-          </Button>
-        </div>
-      </div>
+      ) : null}
     </>
   );
 };
