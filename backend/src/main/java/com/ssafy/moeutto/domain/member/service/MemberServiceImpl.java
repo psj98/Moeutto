c76e0273d2be96f8c30f7c723374ba7db98fcfff
27kanghan <27kanghan@naver.com>
2023-11-10 13:49:39 +0900

    Feat: 로그아웃 수정 및 이메일로 닉네임 반환하는 API 구현
    Former-commit-id: a976563a037f60db6c48969bb20e29803d4de522

--- backend/src/main/java/com/ssafy/moeutto/domain/member/service/MemberServiceImpl.java
+++ backend/src/main/java/com/ssafy/moeutto/domain/member/service/MemberServiceImpl.java
@@ -1,9 +1,14 @@
 package com.ssafy.moeutto.domain.member.service;
 
+import com.ssafy.moeutto.domain.member.entity.Member;
 import com.ssafy.moeutto.domain.member.repository.MemberRepository;
+import com.ssafy.moeutto.global.response.BaseException;
+import com.ssafy.moeutto.global.response.BaseResponseStatus;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Slf4j
 @Service
@@ -11,4 +16,22 @@ import org.springframework.stereotype.Service;
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
+
+    /**
+     * 솔이가 요청한 닉네임 검색 API 입니다.
+     * @param memberId
+     * @param email
+     * @return
+     * @throws BaseException
+     */
+    @Override
+    public String findNicknameForSol(UUID memberId, String email) throws BaseException {
+
+        Member member = memberRepository.findById(memberId).orElseThrow(()-> new BaseException(BaseResponseStatus.NOT_FOUND_MEMBER));
+
+        Member friends = memberRepository.findByEmail(email).orElseThrow(()-> new BaseException(BaseResponseStatus.NOT_FOUND_MEMBER));
+        String nickname = friends.getNickname();
+
+        return nickname;
+    }
 }
