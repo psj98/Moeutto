fab41d39a6a4334080750cb13569bedf75c8f5b6
seoyoung81 <seosue0801@gmail.com>
2023-11-10 14:37:40 +0900

    Feat: props를 전달 한 상태에서만 작동하게 하기
    Former-commit-id: bdacf958e5a4cba2f3fe271f9a629e751759b0cc

--- frontend/src/components/calendar/templates/CalendarTemplates.tsx
+++ frontend/src/components/calendar/templates/CalendarTemplates.tsx
@@ -5,9 +5,6 @@ interface TemplateProps {
     setShowSelectedImg: React.Dispatch<React.SetStateAction<string>>;
     setClothesId: React.Dispatch<React.SetStateAction<number>>;
     setIsLikedOutFit: React.Dispatch<React.SetStateAction<number>>;
-    // setShowSelectedImg: any
-    // setClothesId: any
-    // setIsLikedOutFit: any
     handleModalOpen: () => void;
 }
 
