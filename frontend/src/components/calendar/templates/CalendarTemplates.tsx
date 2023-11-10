720f8036e1ec4ced5a28fe1095f4cd45c042fcf9
seoyoung81 <seosue0801@gmail.com>
2023-11-10 10:04:57 +0900

    Feat: 달련 컴포넌트 분리
    Former-commit-id: ced10da1026288d859281730c4a855e90bb4d82f

--- frontend/src/components/calendar/templates/CalendarTemplates.tsx
+++ frontend/src/components/calendar/templates/CalendarTemplates.tsx
@@ -1,11 +1,19 @@
+import React from "react";
 import Calendar from "../organisms/Calendar";
+
+interface TemplateProps {
+    setShowSelectedImg: React.Dispatch<React.SetStateAction<string>>;
+    setClothesId: React.Dispatch<React.SetStateAction<number>>;
+    setIsLikedOutFit: React.Dispatch<React.SetStateAction<number>>;
+    handleModalOpen: any;
+}
 
 const CalendarTemplates = ({
     setShowSelectedImg,
     setClothesId,
     setIsLikedOutFit,
     handleModalOpen
-}) => {
+}: TemplateProps) => {
     return (
         <div className="">
             <Calendar
