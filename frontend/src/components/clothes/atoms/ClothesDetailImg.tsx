dd5e1105949c47b1c6243d44e4c3ffae842a8ce2
seoyoung81 <seosue0801@gmail.com>
2023-11-12 18:36:57 +0900

    Feat: 디테일 페이지 색상 적용 안되는 것 수정
    Former-commit-id: 5d38e1dfe66a21e0c6c3a5f1b374da6e57323293

--- frontend/src/components/clothes/atoms/ClothesDetailImg.tsx
+++ frontend/src/components/clothes/atoms/ClothesDetailImg.tsx
@@ -14,34 +14,6 @@ const ClothesDetailImg = ({ imgUrl, star, setStar }: PropsType ) => {
         setStar((prev) => !prev)
     }
 
-    // let width: number = 15;
-    // let c: string = "";
-
-    // useEffect(() => {
-    //     const setRightStyle = () => {
-    //         const element = document.querySelector<HTMLElement>(".babo");
-    
-    //         if (element&& window.screen.availWidth > 390) {
-    //             const width = Math.min(document.getElementById("img").clientWidth * 0.3, 50);
-    //             const c = `${width.toString()}px`;
-    
-    //             console.log(c, typeof c, "11111");
-    
-    //             element.style.right = c;
-    //         }else{
-    //             const c = `20px`;
-    
-    //             console.log(c, typeof c, "11111");
-    
-    //             element.style.right = c;
-    //         }
-    //     };
-    
-    //     // Run the function after the initial render to ensure that the DOM is ready.
-    //     setTimeout(setRightStyle, 0);
-    // }, []);
-    
-
     return (
         <>
             <div className="w-full baboParent flex flex-col items-center relative">
@@ -54,10 +26,9 @@ const ClothesDetailImg = ({ imgUrl, star, setStar }: PropsType ) => {
                     <div className={`babo absolute -top-2 w-[60px] right-[20px] sm:right-[30px] md:right-[50px] lg:right-[60px]`}>
                         {star ? (
                                 <RxBookmark size={60} color="FAA0BF" onClick={onClickBookMark} />
-                            
                             ) : (
                                 <RxBookmarkFilled size={60} color="FAA0BF" onClick={onClickBookMark} />
-                                )}
+                            )}
                     </div>
 
                 </div>
