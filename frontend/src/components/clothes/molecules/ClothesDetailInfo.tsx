d3a9639b66988b1c93e1a40ec43df1954846f286
seoyoung81 <seosue0801@gmail.com>
2023-11-12 18:51:49 +0900

    Design: 계절의 개수가 3개 이상일 때,  CSS 높이 수정
    Former-commit-id: 0e9684b1a04d0206d44a82d4daf492c1d04896a5

--- frontend/src/components/clothes/molecules/ClothesDetailInfo.tsx
+++ frontend/src/components/clothes/molecules/ClothesDetailInfo.tsx
@@ -3,15 +3,21 @@ import Content from "../atoms/Content";
 
 interface PropsType {
     labelList: string[];
-    contentList: (string | number)[];
+    // contentList: (string | number)[];
+    contentList: any;
 }
 
 const ClothesDetailInfo = ({ labelList, contentList }: PropsType) => {
+    // const oneCount = contentList[1].filter(char => char === '1').length;
+    
+    // console.log(oneCount)
+    const oneCount: number = 3;
+
     return (
         <div className="flex">
-            <div className="space-y-8 flex flex-col items-end me-10 w-[80px]">
+            <div className="space-y-8 flex flex-col items-end me-10 w-[120px]">
                 {labelList.map((title, index) => (
-                    <LabelContent title={title} index={index} />
+                    <LabelContent title={title} index={index} oneCount={oneCount}  />
                 ) )}
             </div>
             <div className="space-y-8 flex flex-col">
