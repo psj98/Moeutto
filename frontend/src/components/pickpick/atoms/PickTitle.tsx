a303d8593ff8e623bb9406b1ba0ada0d7d586121
27kanghan <27kanghan@naver.com>
2023-11-13 18:09:05 +0900

    Feat: 진행 사항 저장
    Former-commit-id: 2dc985aef36296a683f1e2f49e0d1324c8b1a575

--- frontend/src/components/pickpick/atoms/PickTitle.tsx
+++ frontend/src/components/pickpick/atoms/PickTitle.tsx
@@ -9,7 +9,7 @@ const PickTitle = () => {
   useEffect(() => {
     if (pathname.split('/').length > 2) {
       const email = pathname.split('/')[3]; // ['', 'notmycloset', 'friend', 'email']\
-      
+
       const fetchData = async () => {
         try {
           // 토큰이 필요한 api의 경우 authInstance를 가져옵니다
@@ -17,7 +17,7 @@ const PickTitle = () => {
           const response = await axiosInstance.post('/members/find-nickname', {
             email, // 친구 email
           });
-    
+
           if (response.data.data) {
             setNickname(response.data.data);
           } else {
@@ -28,18 +28,24 @@ const PickTitle = () => {
           throw new Error('친구 닉네임 조회 실패');
         }
       };
-      
+
       fetchData();
     } else {
       setNickname(sessionStorage.getItem(nickname));
       // 빈 값인 경우 나의 옷장이다
       if (!nickname) {
-        setNickname('나')
+        setNickname('나');
       }
     }
-  }, [])
+  }, []);
 
-  return <div className="text-[28px] font-bold tracking-wider">{nickname}의 옷장</div>;
+  return (
+    <div className="mt-[30px] text-center">
+      <div className="shadow-md max-w-md mx-auto p-4 rounded-lg bg-white text-black text-4xl font-extrabold theJamsil">
+        <h1>{nickname}의 옷장</h1>
+      </div>
+    </div>
+  );
 };
 
 export default PickTitle;
