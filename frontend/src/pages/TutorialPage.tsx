c548345b676294d735e219e9d61fde3a04590ef6
27kanghan <27kanghan@naver.com>
2023-11-13 12:43:18 +0900

    Feat: 슬라이드 마지막 페이지에 버튼 내용 수정
    Former-commit-id: 10b7dcfbf4983aae8782d31bcdd16d2b83544dc6

--- frontend/src/pages/TutorialPage.tsx
+++ frontend/src/pages/TutorialPage.tsx
@@ -1,28 +1,37 @@
-// import React, { useRef } from 'react';
-
-import { Pagination, Navigation, Autoplay } from 'swiper/modules';
-
+import React, { useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Navigation, Autoplay } from 'swiper/modules';
 import { useNavigate } from 'react-router-dom';
+
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/autoplay';
 
-import tutorialImageFirst from '../assets/images/tutoral_1.png';
+// 이미지 임포트
+import tutorialImageFirst from '../assets/images/tutorial/recommend.png';
+import friendsCloset from '../assets/images/tutorial/friends-closet.png';
+import scoreMyOutfit from '../assets/images/tutorial/pickpick.png';
 
 const TutorialPage = () => {
-  const naviagte = useNavigate();
+  const navigate = useNavigate();
+  const [currentSlideClass, setCurrentSlideClass] = useState('');
 
   const goMainPage = () => {
-    naviagte('/main');
+    navigate('/main');
+  };
+
+  const handleSlideChange = swiper => {
+    const currentSlide = swiper.slides[swiper.activeIndex];
+
+    setCurrentSlideClass(currentSlide.className);
   };
 
   return (
     <div className="h-[full]]">
-      <div className="mb-[200px]"></div>
+      <div className="mb-[40px]"></div>
       <div className="h-[500px]">
         <Swiper
-          className="mt-[125px] mb-[90px] "
+          className="mb-[90px]"
           modules={[Pagination, Navigation, Autoplay]}
           spaceBetween={100}
           navigation
@@ -32,45 +41,49 @@ const TutorialPage = () => {
           }}
           pagination={{ clickable: true }}
           onSwiper={swiper => console.log(swiper)}
-          onSlideChange={() => console.log('slide change')}>
+          onSlideChange={handleSlideChange}>
           <SwiperSlide>
             <div className="">
-              <img src={tutorialImageFirst} className="w-full h-auto object-cover"></img>
+              <img src={tutorialImageFirst} className="w-full h-[500px]" alt="Tutorial First"></img>
             </div>
-            <div className="text-center mt-[30px]">날씨에 맞는 옷을 자동으로 추천해줍니다.</div>
+            <div className="text-center mt-[30px]">내 옷장을 모으또에 등록하세요!</div>
             <div className="mt-[50px]"></div>
           </SwiperSlide>
-
           <SwiperSlide>
             <div className="">
-              <img src={tutorialImageFirst} className="w-full h-auto object-cover"></img>
+              <img src={tutorialImageFirst} className="w-full h-[500px]" alt="Tutorial Weather"></img>
             </div>
-            <div className="text-center mt-[30px]">날씨에 맞는 옷을 자동으로 추천해줍니다.</div>
+            <div className="text-center mt-[30px]">내 옷장에서 날씨에 맞는 옷을 추천해줍니다.</div>
             <div className="mt-[50px]"></div>
           </SwiperSlide>
-
           <SwiperSlide>
             <div className="">
-              <img src={tutorialImageFirst} className="w-full h-auto object-cover"></img>
+              <img src={scoreMyOutfit} className="w-full h-[500px]" alt="Score My Outfit"></img>
             </div>
-            <div className="text-center mt-[30px]">날씨에 맞는 옷을 자동으로 추천해줍니다.</div>
+            <div className="text-center mt-[30px]">오늘 입을 옷이 적절한지 평가해보세요!</div>
             <div className="mt-[50px]"></div>
           </SwiperSlide>
-
           <SwiperSlide>
             <div className="">
-              <img src={tutorialImageFirst} className="w-full h-auto object-cover"></img>
+              <img src={friendsCloset} className="h-[500px] w-full" alt="Friends Closet"></img>
             </div>
-            <div className="text-center mt-[30px]">날씨에 맞는 옷을 자동으로 추천해줍니다.</div>
+            <div className="text-center mt-[30px]">친구의 옷장을 구경하고 댓글을 달아보세요.</div>
+            <div className="mt-[50px]"></div>
+          </SwiperSlide>
+          <SwiperSlide className="lastPage">
+            <div className="">
+              <img src={friendsCloset} className="h-[500px] w-full" alt="Friends Closet Last"></img>
+            </div>
+            <div className="text-center mt-[30px]">친구의 옷장을 구경하고 댓글을 달아보세요.</div>
             <div className="mt-[50px]"></div>
           </SwiperSlide>
         </Swiper>
       </div>
-      <div className="flex justify-center ">
+      <div className="flex justify-center mt-[150px]">
         <button
           onClick={goMainPage}
           className="bg-pink-hot rounded-xl shadow-xl text-white text-WebBody3 p-4 tracking-wider font-bold">
-          건너뛰기
+          {currentSlideClass.includes('lastPage') ? '시작하기' : '건너뛰기'}
         </button>
       </div>
     </div>
