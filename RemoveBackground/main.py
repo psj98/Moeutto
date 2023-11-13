026c5ee76cbe8fa6f0b383cdc756dac4398928c8
hannernos <hyeon.dh.kim@gmail.com>
2023-11-14 03:34:19 +0900

    Feat : category
    Former-commit-id: 2ff9f34835f8eda34b65a94b5a67b116c78aacc9

--- RemoveBackground/main.py
+++ RemoveBackground/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, File, UploadFile
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 
 import process as kjg
 from PIL import Image
@@ -10,6 +11,14 @@ from color import closest_color
 from fastapi.responses import JSONResponse,StreamingResponse
 import base64
 
+from classification.classification import load_model, predict_image  # classification 폴더에서 classification 모듈 임포트
+
+
+seg_model_path = 'model/cloth_segm.pth'
+seg_model = kjg.load_seg_model(seg_model_path, device='cpu')
+
+classification_model_path = './classification/fashion_cnn.pth'
+classification_model = load_model(classification_model_path)
 
 
 app = FastAPI()
@@ -39,33 +48,27 @@ async def create_report(file: UploadFile = File(...)):
     try:
         img = await file.read()
         img = Image.open(BytesIO(img)).convert('RGB')
-        # img = Image.open(BytesIO(content)).convert('RGB')
-        #  이 사이에서 문제 발생
 
-        # print("img Type : "+str(type(img)))
-        ret = kjg.main(img)
-        # print("predict 완료")
-        # print("ret type:"+str(type(ret)))
+        # 배경 제거
+        ret = kjg.main(img, seg_model)
+
+        # 색상 추출
         width, height = ret.size
         center_pixel = ret.getpixel((width // 2, height // 2))
         hex_color = '#{:02x}{:02x}{:02x}'.format(center_pixel[0], center_pixel[1], center_pixel[2])
         str_color = closest_color(hex_color)
 
-        # ret tpye:<class 'PIL.Image.Image'>
-        # ret-> segmented img
-        # return {'file': ret}
+        # 이미지 인코딩
         buffer = BytesIO()
         ret.save(buffer, format='JPEG')
         buffer.seek(0)
         encoded_image = base64.b64encode(buffer.getvalue()).decode()
-
-        # JSON 응답으로 이미지와 색상 정보 반환
-        # multipart response
         # return StreamingResponse(buffer, media_type="image/jpeg")
 
-        # 분류
-        category = "top"
-
+        # category(분류)
+        # category = "top"
+        buffer.seek(0)
+        category = predict_image(buffer.getvalue(), classification_model)
 
         return JSONResponse(content={"file": encoded_image, "color": str_color, "category" : category})
 
