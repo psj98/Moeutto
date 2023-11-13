b81368bd328c5473a510cb6ffb07beb17c33dac0
seoyoung81 <seosue0801@gmail.com>
2023-11-13 16:03:56 +0900

    Feat: 이름이 없는 경우 comment가 비어 있기 때문에 색상+카테고리 조합으로 새로운 이름 만들기
    Former-commit-id: e4b55211465b810111c256e1f5d9f9cc942c40c3

--- frontend/src/components/clothes/atoms/LableContent.tsx
+++ frontend/src/components/clothes/atoms/LableContent.tsx
@@ -2,11 +2,15 @@ interface PropsType {
     title: string;
     index: number;
     oneCount: number;
+    price: number;
+    brand: string;
 }
 
-const LabelContent = ({ title, index, oneCount }: PropsType) => {
+const LabelContent = ({ title, index, oneCount, price, brand }: PropsType) => {
     return (
-        <div className={`text-AppBody1 font-bold ${index === 3 ? 'h-[35px]' : ''} ${oneCount >= 3 && index === 1 ? 'h-[75px]' : ''}`}>{title}</div>
+        <div className={`text-AppBody1 font-bold ${index === 3 ? 'h-[35px]' : ''} ${oneCount >= 3 && index === 1 ? 'h-[75px]' : ''}`}>
+            {!(index === 5 && price === 0) && !(index === 6 && brand === '') && title}
+        </div>
     )
 }
 
