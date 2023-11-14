fa09175e05a2db9d62d3191137e92fe695ec5b56
김동현 <hyeon.dh.kim@gmail.com>
2023-11-14 13:35:02 +0900

    Feat : Color
    Former-commit-id: ea72b51b98d9211fa3bbb5c58513375bc38e11a4

--- RemoveBackground/color.py
+++ RemoveBackground/color.py
@@ -1,5 +1,19 @@
 from math import sqrt
 
+color_palette = [
+    {'name': 'red', 'kr': '빨강', 'background': '#FF0000'},
+    {'name': 'orange', 'kr': '주황', 'background': '#FFA500'},
+    {'name': 'yellow', 'kr': '노랑', 'background': '#FFFF00'},
+    {'name': 'green', 'kr': '초록', 'background': '#008000'},
+    {'name': 'blue', 'kr': '파랑', 'background': '#0000FF'},
+    {'name': 'purple', 'kr': '보라', 'background': '#800080'},
+    {'name': 'pink', 'kr': '핑크', 'background': '#FFC0CB'},
+    {'name': 'khaki', 'kr': '카키', 'background': '#F0E68C'},
+    {'name': 'black', 'kr': '검정', 'background': '#000000'},
+    {'name': 'white', 'kr': '하양', 'background': '#FFFFFF'},
+    {'name': 'grey', 'kr': '회색', 'background': '#808080'}
+]
+
 def hex_to_rgb(hex_color):
     hex_color = hex_color.lstrip('#')
     hlen = len(hex_color)
@@ -9,22 +23,10 @@ def color_distance(rgb1, rgb2):
     r_diff = rgb1[0] - rgb2[0]
     g_diff = rgb1[1] - rgb2[1]
     b_diff = rgb1[2] - rgb2[2]
-    return sqrt(r_diff**2 + g_diff**2 + b_diff**2)
+    return sqrt((r_diff**2) * 0.299 + (g_diff**2) * 0.587 + (b_diff**2) * 0.114)
+
 
 def closest_color(hex_color):
-    color_palette = [
-        {'name': 'red', 'kr': '빨강', 'background': '#FFA7A7'},
-        {'name': 'orange', 'kr': '주황', 'background': '#FFA7A7'},
-        {'name': 'yellow', 'kr': '노랑', 'background': '#FDFF9E'},
-        {'name': 'green', 'kr': '초록', 'background': '#A0FF90'},
-        {'name': 'blue', 'kr': '파랑', 'background': '#BEADFF'},
-        {'name': 'purple', 'kr': '보라', 'background': '#D09AD9'},
-        {'name': 'pink', 'kr': '핑크', 'background': '#FF98D6'},
-        {'name': 'khaki', 'kr': '카키', 'background': '#C0AE6F'},
-        {'name': 'black', 'kr': '검정', 'background': '#131313'},
-        {'name': 'white', 'kr': '하양', 'background': '#FFF'},
-        {'name': 'grey', 'kr': '회색', 'background': '#E2E2E2'}
-    ]
 
     input_rgb = hex_to_rgb(hex_color)
     min_distance = float('inf')
