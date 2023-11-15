2f57a1c452a47641dd4734ad044e9f639dfc8f50
s01k1m <sk618dev@gmail.com>
2023-11-15 22:29:17 +0900

    Feat: 계절 페이지 2차 완성
    Former-commit-id: fa3a3c76ea9450bdbf09dcbf9585d2e4c6cbc8e5

--- frontend/src/components/report/organisms/ClosetReportSeasonOrganism.tsx
+++ frontend/src/components/report/organisms/ClosetReportSeasonOrganism.tsx
@@ -1,10 +1,49 @@
-import { SeasonDataType } from '../../../pages/ReportSeasonPage';
+import { useState, useEffect } from 'react';
+import { SeasonDataType, SeasonClothesType } from '../../../pages/ReportSeasonPage';
 import SeasonReportSection from '../molecules/SeasonReportSection';
 import BuyRecommendSection from '../molecules/BuyRecommendSection';
 import IntroComment from '../atoms/IntroComment';
 import ShortReportComment from '../atoms/ShortReportComment';
+import ReportComment from '../atoms/ReportComment';
 
 const ClosetReportSeasonOrganism = ({ fourSeason }: SeasonDataType) => {
+  const [mostSeason, setMostSeason] = useState('');
+
+  function findMaxAmountSeason(seasonClothes: SeasonClothesType): string | null {
+    let maxAmount = 0;
+    let maxAmountSeason: string | null = null;
+
+    // Iterate through each season
+    for (const season in seasonClothes) {
+      if (Object.prototype.hasOwnProperty.call(seasonClothes, season)) {
+        // Calculate the total amount for the current season
+        const totalAmount = seasonClothes[season].reduce((acc, category) => acc + category.amount, 0);
+
+        // Update maxAmountSeason if the current season has a higher total amount
+        if (totalAmount > maxAmount) {
+          maxAmount = totalAmount;
+          maxAmountSeason = season;
+        }
+      }
+    }
+
+    return maxAmountSeason;
+  }
+
+  useEffect(() => {
+    const max = findMaxAmountSeason(fourSeason);
+
+    if (max === 'springClothes') {
+      setMostSeason('봄');
+    } else if (max === 'summerClothes') {
+      setMostSeason('여름');
+    } else if (max === 'autumnClothes') {
+      setMostSeason('가을');
+    } else if (max === 'winterClothes') {
+      setMostSeason('겨울');
+    }
+  }, []);
+
   return (
     <div>
       {/* 인트로 분석 문구 */}
@@ -13,11 +52,22 @@ const ClosetReportSeasonOrganism = ({ fourSeason }: SeasonDataType) => {
       {/* 간단 분석 문구 */}
       <ShortReportComment
         imageDivClass="absolute top-[-35px] left-[10px]"
-        imageUrl="/images/earth.png"
-        imageClass="w-20 inline-block"
-        mainTitle="당신은 지구지킴이?"
-        subTitle="6개월을 기준으로 계산해봤어요"
+        imageUrl={'/images/tree.png'}
+        imageClass="w-16 inline-block"
+        mainTitle={`당신의 옷장은 \n어떤 계절일까요?`}
       />
+
+      {/* 많이 입은 옷 */}
+      <div className="mb-4">
+        <ReportComment
+          divPadding=""
+          imageUrl="/images/report-happy.png"
+          imageClass="w-20 inline-block"
+          mainTitle={`${sessionStorage.getItem('nickname')}님의 옷장에는`}
+          subTitle={`${mostSeason}옷이 가장 많네요`}
+        />
+      </div>
+
       <div className="mt-10 font-extrabold">당신의 옷장 계절은 어떨까요?</div>
       <div className="flex flex-col items-center">
         <SeasonReportSection fourSeason={fourSeason} />
