8fa532740bcbb53e7c8ddd9f4ed4fdf5ba1e8221
s01k1m <sk618dev@gmail.com>
2023-11-16 13:17:58 +0900

    Design: tip 옆 마진 추가
    Former-commit-id: b66777d6f7d487056c2afdb8c753e9ca2f4d06a8

--- frontend/src/components/main/atoms/TodayDate.tsx
+++ frontend/src/components/main/atoms/TodayDate.tsx
@@ -1,12 +1,10 @@
 const TodayDate = () => {
-    // 오늘 날짜
-    const today = new Date();
-    // 형식 맞추기
-    const formattedDate = `${today.getFullYear()}년 ${today.getMonth() + 1}월 ${today.getDate()}일`;
-    
-    return (
-        <div className="font-bold text-AppBody1 ms-6">{formattedDate}</div>
-    )
-}
+  // 오늘 날짜
+  const today = new Date();
+  // 형식 맞추기
+  const formattedDate = `${today.getFullYear()}년 ${today.getMonth() + 1}월 ${today.getDate()}일`;
+
+  return <div className="text-AppBody1 ms-6">{formattedDate}</div>;
+};
 
 export default TodayDate;
