1e1a867ea8cf56e8fed676992b4f7aebcc44fd95
s01k1m <sk618dev@gmail.com>
2023-11-16 13:29:13 +0900

    Design: 온도 up down 아이콘 추가하고 date 영역 2px 축소
    Former-commit-id: c52f05489b045e858f7b340d9adf0babdaf16bd6

--- frontend/src/components/main/atoms/TodayDate.tsx
+++ frontend/src/components/main/atoms/TodayDate.tsx
@@ -4,7 +4,7 @@ const TodayDate = () => {
   // 형식 맞추기
   const formattedDate = `${today.getFullYear()}년 ${today.getMonth() + 1}월 ${today.getDate()}일`;
 
-  return <div className="text-AppBody1 ms-6">{formattedDate}</div>;
+  return <div className="text-[18px] ms-6">{formattedDate}</div>;
 };
 
 export default TodayDate;
