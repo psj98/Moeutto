9e44d2b139e8d4e4137da707f457aff36c4fcf01
김동현 <hyeon.dh.kim@gmail.com>
2023-11-17 09:13:47 +0900

    Fix : RecommendOutfit logic
    Former-commit-id: 48e98bc0c181a5b6d3edc01d8d2db90c46fef7c3

--- RecommendOutfit/myapp/internal/app/logic/logic.go
+++ RecommendOutfit/myapp/internal/app/logic/logic.go
@@ -7,9 +7,10 @@ import (
 	"time"
 )
 
-// SelectClothes - 날씨 정보와 옷 목록을 바탕으로 최적의 옷을 선택하여 추천 결과를 생성
+// SelectClothes - 날씨 정보와 옷 목록을 바탕으로 최적의 옷을 선택하여 추천 결과를 생성합니다.
 func SelectClothes(clothesList models.ClothesList, weatherInfos []models.WeatherInfo) []models.LogicRecommendation {
 	var recommendations []models.LogicRecommendation
+	recentlyRecommended := make(map[int]bool)
 
 	for _, weather := range weatherInfos {
 		recommendation := models.LogicRecommendation{
@@ -17,15 +18,16 @@ func SelectClothes(clothesList models.ClothesList, weatherInfos []models.Weather
 			RecDate:   weather.Date,
 		}
 
-		outerClothesId, outerIndex := selectClothForCategory(clothesList.Outer, weather)
-		topClothesId, topIndex := selectClothForCategory(clothesList.Top, weather)
-		bottomClothesId, bottomIndex := selectClothForCategory(clothesList.Bottom, weather)
-		itemClothesId, itemIndex := selectClothForCategory(clothesList.Item, weather)
+		// 각 카테고리별로 최적의 옷을 선택합니다.
+		for _, category := range [][]models.Clothes{clothesList.Outer, clothesList.Top, clothesList.Bottom, clothesList.Item} {
+			selectedClothId := selectBestCloth(category, weather, recentlyRecommended)
+			recommendation.ClothesId = append(recommendation.ClothesId, selectedClothId)
+		}
 
-		recommendation.ClothesId = append(recommendation.ClothesId, outerClothesId, topClothesId, bottomClothesId, itemClothesId)
-
-		// 각 카테고리별로 ClothesId에 해당하는 옷의 빈도와 최근 착용 날짜 업데이트
-		updateClothesUsage(&clothesList, []int{outerIndex, topIndex, bottomIndex, itemIndex}, weather.Date)
+		// 최근 추천된 아이템을 업데이트합니다.
+		for _, id := range recommendation.ClothesId {
+			recentlyRecommended[id] = true
+		}
 
 		recommendations = append(recommendations, recommendation)
 	}
@@ -33,77 +35,67 @@ func SelectClothes(clothesList models.ClothesList, weatherInfos []models.Weather
 	return recommendations
 }
 
-// selectClothForCategory - 특정 카테고리의 옷 중에서 최적의 옷을 선택
-func selectClothForCategory(clothes []models.Clothes, weather models.WeatherInfo) (int, int) {
-	// 현재 날짜 구하기
-	currentTime, _ := time.Parse("2006-01-02", weather.Date)
-	yesterday := currentTime.AddDate(0, 0, -1).Format("2006-01-02")
-	dayBeforeYesterday := currentTime.AddDate(0, 0, -2).Format("2006-01-02") // 2일 전 날짜 추가
+// selectBestCloth - 특정 카테고리의 옷 중에서 날씨와 최근 사용 정보를 바탕으로 최적의 옷을 선택합니다.
+func selectBestCloth(clothes []models.Clothes, weather models.WeatherInfo, recentlyRecommended map[int]bool) int {
+	if len(clothes) == 0 {
+		return -1 // 해당 카테고리에 옷이 없는 경우
+	}
 
-	// 착용 횟수, 최근 착용 날짜, 계절 고려 정렬
+	// 옷을 점수에 따라 정렬합니다.
 	sort.SliceStable(clothes, func(i, j int) bool {
-		// 계절 부합성 확인
-		isSeasonMatchI := isSeasonMatch(clothes[i].Season, determineSeason(weather)) //bool
-		isSeasonMatchJ := isSeasonMatch(clothes[j].Season, determineSeason(weather)) //bool
-
-		// 최근 착용 날짜 확인
-		recentWornI := clothes[i].RecentDate == weather.Date || clothes[i].RecentDate == yesterday || clothes[i].RecentDate == dayBeforeYesterday
-		recentWornJ := clothes[j].RecentDate == weather.Date || clothes[j].RecentDate == yesterday || clothes[j].RecentDate == dayBeforeYesterday
-
-		// 착용 횟수 확인
-		frequentWornI := clothes[i].Frequency >= 30
-		frequentWornJ := clothes[j].Frequency >= 30
-
-		// 조건별 우선 순위 결정
-		if frequentWornI != frequentWornJ {
-			return !frequentWornI // 착용 횟수가 30회 미만인 옷 우선
-		} else if recentWornI != recentWornJ {
-			return !recentWornI // 최근에 착용하지 않은 옷 우선
-		} else if isSeasonMatchI != isSeasonMatchJ {
-			return isSeasonMatchI // 계절에 맞는 옷 우선
-		} else {
-			return clothes[i].Frequency > clothes[j].Frequency // 나머지 경우 착용 횟수가 많은 옷 우선
-		}
+		return scoreClothingItem(clothes[i], weather, recentlyRecommended) > scoreClothingItem(clothes[j], weather, recentlyRecommended)
 	})
 
-	// 최적의 옷 선택
-	if len(clothes) > 0 {
-		return clothes[0].ClothesId, 0
+	return clothes[0].ClothesId
+}
+
+// scoreClothingItem - 날씨, 착용 빈도, 최근 사용 여부에 따라 각 옷에 점수를 매깁니다.
+func scoreClothingItem(cloth models.Clothes, weather models.WeatherInfo, recentlyRecommended map[int]bool) int {
+	score := 0
+
+	// 계절에 부합하는지 확인
+	if isSeasonMatch(cloth.Season, determineSeason(weather)) {
+		score += 20
 	}
 
-	return -1, -1
-}
-
-// updateClothesUsage - 옷의 사용 정보 업데이트
-func updateClothesUsage(clothesList *models.ClothesList, indices []int, date string) {
-	categories := []*[]models.Clothes{&clothesList.Outer, &clothesList.Top, &clothesList.Bottom, &clothesList.Item}
-	for i, index := range indices {
-		if index != -1 {
-			category := categories[i]
-			(*category)[index].Frequency++
-			(*category)[index].RecentDate = date
-		}
+	// 온도 적합성에 따라 점수를 조정
+	avgTemp := (weather.Tmx + weather.Tmn) / 2
+	switch {
+	case avgTemp >= 20 && cloth.Thickness == 1:
+		score += 15
+	case avgTemp < 20 && avgTemp >= 10 && cloth.Thickness == 2:
+		score += 15
+	case avgTemp < 10 && cloth.Thickness == 3:
+		score += 15
 	}
-}
 
-// filterByWeather - 날씨 조건에 맞는 옷을 필터링
-func filterByWeather(clothes []models.Clothes, weather models.WeatherInfo) []models.Clothes {
-	var filtered []models.Clothes
-	for _, cloth := range clothes {
-		if isSuitableForWeather(cloth, weather) {
-			filtered = append(filtered, cloth)
-		}
+	// 착용 빈도에 따라 점수 조정 (적게 착용할수록 높은 점수)
+	score += max(0, 10-cloth.Frequency)
+
+	// 최근 착용 날짜에 따라 점수를 조정
+	if daysSince(cloth.RecentDate) > 30 {
+		score += 10
+	} else {
+		score -= daysSince(cloth.RecentDate) / 3
 	}
-	return filtered
+
+	// 최근 추천된 아이템은 점수를 감점합니다.
+	if _, found := recentlyRecommended[cloth.ClothesId]; found {
+		score -= 20
+	}
+
+	return score
 }
 
-// isSuitableForWeather - 날씨 조건에 따라 적합한 옷을 판별
-func isSuitableForWeather(cloth models.Clothes, weather models.WeatherInfo) bool {
-	season := determineSeason(weather)
-	return isSeasonMatch(cloth.Season, season) && isThicknessSuitable(cloth.Thickness, weather)
+// isSeasonMatch - 옷의 계절 정보와 결정된 계절이 일치하는지 확인합니다.
+func isSeasonMatch(clothesSeason, currentSeason string) bool {
+	clothesSeasonBits, _ := strconv.ParseInt(clothesSeason, 2, 64)
+	currentSeasonBits, _ := strconv.ParseInt(currentSeason, 2, 64)
+
+	return (clothesSeasonBits & currentSeasonBits) > 0
 }
 
-// determineSeason - 날씨 정보를 바탕으로 계절을 결정
+// determineSeason - 날씨 정보를 바탕으로 계절을 결정합니다.
 func determineSeason(weather models.WeatherInfo) string {
 	date, _ := time.Parse("2006-01-02", weather.Date)
 	month := date.Month()
@@ -120,24 +112,17 @@ func determineSeason(weather models.WeatherInfo) string {
 	}
 }
 
-// isSeasonMatch - 옷의 계절 정보와 결정된 계절이 일치하는지 확인
-func isSeasonMatch(clothesSeason, currentSeason string) bool {
-	clothesSeasonBits, _ := strconv.ParseInt(clothesSeason, 2, 64)
-	currentSeasonBits, _ := strconv.ParseInt(currentSeason, 2, 64)
-
-	return (clothesSeasonBits & currentSeasonBits) > 0
+// daysSince - 주어진 날짜로부터 경과한 일 수를 계산합니다.
+func daysSince(dateStr string) int {
+	today := time.Now()
+	pastDate, _ := time.Parse("2006-01-02", dateStr)
+	return int(today.Sub(pastDate).Hours() / 24)
 }
 
-// isThicknessSuitable - 옷의 두께가 현재 날씨 조건에 적합한지 판별
-func isThicknessSuitable(thickness int, weather models.WeatherInfo) bool {
-	avgTemp := (weather.Tmx + weather.Tmn) / 2
-
-	switch {
-	case avgTemp >= 20:
-		return thickness <= 1 // 더운 날씨에는 얇은 옷
-	case avgTemp >= 10:
-		return thickness <= 2 // 중간 날씨에는 중간 두께
-	default:
-		return thickness >= 3 // 추운 날씨에는 두꺼운 옷
-	}
-}
+// max
+//func max(a, b int) int {
+//	if a > b {
+//		return a
+//	}
+//	return b
+//}
