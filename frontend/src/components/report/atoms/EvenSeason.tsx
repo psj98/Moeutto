30438b9ddcf8fbedf860e94f656954eb33694362
s01k1m <sk618dev@gmail.com>
2023-11-17 15:12:57 +0900

    Fix: amount와 name mapping 로직 수정
    Former-commit-id: 619e9179b6b031933db42d83bf4a9b44cd288470

--- frontend/src/components/report/atoms/EvenSeason.tsx
+++ frontend/src/components/report/atoms/EvenSeason.tsx
@@ -35,19 +35,41 @@ const Card = styled.div`
 `;
 
 const EvenSeason = ({ season, name, month }: SeasonData) => {
-  const result2 = largeCategory
-    .filter(item => {
-      return season.find(c => c.largeCategoryId === item.id) !== undefined;
-    })
-    .map((item, index) => {
-      const amount = season[index]?.amount;
+  const result1 = largeCategory.filter(item => {
+    // season 배열의 각 요소 중 largeCategoryId가 현재 아이템의 id와 일치하는지 확인합니다.
+    return season.find(c => c.largeCategoryId === item.id) !== undefined; // id를 한글 이름으로 맵핑한다
+  });
 
-      return {
-        name: item.name, // 예시로 'name'을 가져옴
-        amount, // 여기에서 실제로 'amount' 값을 가져와야 함
-      };
-    });
+  /*
+   result1의 예상값 [
+    {
+      id: '002',
+      name: '아우터',
+    },
+  ]
+  result1이 largeCategory와 똑같아 보이는데 이러한 이유로 쓴다.
+  1. season 배열에 대응되는 largeCategory 아이템만을 추출
+  2. 해당 아이템에 대한 추후 추가적인 가공을 수행하기 위해서 사용 가능성.
+  */
 
+  const result2 = result1.map(item1 => {
+    const matchingSeasonItem = season.find(c => c.largeCategoryId === item1.id);
+
+    // matchingSeasonItem이 존재하면 해당 아이템에 대한 새로운 객체를 반환합니다.
+    // 존재하지 않으면 { name: item1.name, amount: undefined }를 반환합니다.
+    return {
+      name: item1.name,
+      amount: matchingSeasonItem ? matchingSeasonItem.amount : 0,
+    };
+  });
+
+  /*
+  result2 의 결과값
+   [{name: '아우터', amount: 1},
+  {name: '상의', amount: 3},
+  {name: '하의', amount: 2},
+  {name: '아이템', amount: 0}]
+  */
   return (
     <Card
       style={{
