a841741d76836f8f3670e0bba7fe4c00bddad530
seoyoung81 <seosue0801@gmail.com>
2023-11-17 13:23:24 +0900

    Feat: 카테고리 수정 중
    Former-commit-id: 6c70c5ab1572cc115128117bd17683590b77fec6

--- frontend/src/components/common/category/organisms/PickComponent.tsx
+++ frontend/src/components/common/category/organisms/PickComponent.tsx
@@ -14,8 +14,6 @@ interface PropsType {
   handleSubmit: React.MouseEventHandler<HTMLButtonElement>;
   clothesData: ClothesItem[];
   nickname?: string;
-  // 이건 모두 있어야 할 값 같은데 리팩토링 필요
-  isImgLoading?: boolean;
 }
 
 const PickComponent: React.FC<PropsType> = ({
@@ -27,7 +25,6 @@ const PickComponent: React.FC<PropsType> = ({
   setSelectedOptionSort,
   handleSubmit,
   clothesData,
-  isImgLoading
 }) => {
   return (
     <>
@@ -42,7 +39,6 @@ const PickComponent: React.FC<PropsType> = ({
       />
       <div className="flex justify-center m-auto">
         <div className="w-[95%] flex flex-wrap gap-3.5 mt-4 justify-start mb-[200px]">
-        {!isImgLoading ? (
           <>
             {clothesData && clothesData.length > 0 ? (
               clothesData.map((item, index) => (
@@ -57,13 +53,6 @@ const PickComponent: React.FC<PropsType> = ({
               <div>아무것도 없어요</div>
             )}
           </>
-        ) : (
-          <div className='flex gap-4 justify-center'>
-            <div className="animate-pulse w-[110px] h-[110px] bg-gray-button rounded-3xl"></div>
-            <div className="animate-pulse w-[110px] h-[110px] bg-gray-button rounded-3xl"></div>
-            <div className="animate-pulse w-[110px] h-[110px] bg-gray-button rounded-3xl"></div>
-          </div>
-        )}
         </div>
       </div>
     </>
