86320ae91aaf370f6da2d34a05ab0eb657ca5506
seoyoung81 <seosue0801@gmail.com>
2023-11-19 16:11:42 +0900

    Chore: 안쓰는 코드 삭제
    Former-commit-id: aed43dc68b0435b927c448d2d544e19dcc6b321e

--- frontend/src/components/main/atoms/TempBarAnimation.tsx
+++ frontend/src/components/main/atoms/TempBarAnimation.tsx
@@ -2,39 +2,31 @@ import React, { useEffect, useState } from "react";
 
 const TempBarAnimation = () => {
     const [position, setPosition] = useState<number>(0);
-    const [count, setCount] = useState<number>(0);
 
     useEffect(() => {
       const interval = setInterval(() => {
-        setCount(prevCount => {
-          if (prevCount < 3) {
-            setPosition(prevPosition => (prevPosition === 80 ? 0 : 80));
-            return prevCount + 1;
-          } else {
-            clearInterval(interval);
-            return prevCount;
-          }
-        });
-      }, 2000); // 1초 간격으로 변경
+        setPosition(prevPosition => (prevPosition === 80 ? 0 : 80));
+      }, 2000); // 2초 간격으로 변경
 
       return () => clearInterval(interval);
     }, []);
     
     return (
         <div className="flex items-center justify-center mt-10 mb-6">
-                <div className="bg-gray-button w-full h-[80px] rounded-[30px] flex justify-center items-center">
+            <div className="bg-gray-button w-full h-[80px] rounded-[30px] flex justify-center items-center">
                 <img src="/images/bright.png" alt="밝음" />
                 <div className="w-[230px] h-[17px] rounded-2xl shadow-lg bg-white m-2">
                     <div
-                    className="w-7 h-5 bg-pink rounded-full relative"
-                    style={{
-                        left: `${position}%`,
-                        transition: 'left 2s ease-in-out'
-                    }}></div>
+                        className="w-7 h-5 bg-pink rounded-full relative"
+                        style={{
+                            left: `${position}%`,
+                            transition: 'left 2s ease-in-out'
+                        }}
+                    ></div>
                 </div>
                 <img src="/images/dark.png" alt="어두움" />
-                </div>
             </div>
+        </div>
     )
 }
 
