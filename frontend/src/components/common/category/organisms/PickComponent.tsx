9bde506fab941269d73c5704b628d345facfdbbd
s01k1m <sk618dev@gmail.com>
2023-11-19 19:41:50 +0900

    Design: pickpick 페이지 컴포넌트 가운데 정렬
    Former-commit-id: ee2565cf87ebf271e2e0913badb2f60624decafd

--- frontend/src/components/common/category/organisms/PickComponent.tsx
+++ frontend/src/components/common/category/organisms/PickComponent.tsx
@@ -37,8 +37,8 @@ const PickComponent: React.FC<PropsType> = ({
         setSelectedOptionSort={setSelectedOptionSort}
         handleSubmit={handleSubmit}
       />
-      <div className="flex justify-center m-auto">
-        <div className="w-[95%] flex flex-wrap gap-3.5 mt-4 justify-start mb-[200px]">
+      <div className="flex justify-center items-center">
+        <div className="min-[375px]:w-[98%] min-[400px]:w-[90%] min-[500px]:w-[85%] flex flex-wrap gap-3.5 mt-4 justify-between mb-[200px]">
           <>
             {clothesData && clothesData.length > 0 ? (
               clothesData.map((item, index) => (
