258f67a8b199a8dc6c84528ac8006d01b0268f15
s01k1m <sk618dev@gmail.com>
2023-11-19 19:33:09 +0900

    Feat: pickpick 리덕스 상태 초기화 로직 추가
    Former-commit-id: 8e1b6a82bd140cc779ab8a99974f951852e0181a

--- frontend/src/redux/features/closet/selectClosetSlice.ts
+++ frontend/src/redux/features/closet/selectClosetSlice.ts
@@ -52,21 +52,25 @@ const selectClosetSlice = createSlice({
       const index = selectedClosetIds.findIndex(cloth => cloth.id === clothesId.id);
 
       // 리셋하는 로직
-      if (action.payload.id === '0') {
+      if (clothesId.largeCategoryId === 'init') {
         state.selectedClosetIds = [];
       }
-      if (index === -1) {
+
+      if (index === -1 && clothesId.largeCategoryId !== 'init') {
         // 리스트에 없으면 추가
         state.selectedClosetIds = [...selectedClosetIds, clothesId];
       } else {
         // 리스트에 있으면 삭제
         state.selectedClosetIds = selectedClosetIds.filter(cloth => cloth.id !== clothesId.id);
       }
-      console.log(selectedClosetIds);
+    },
+    // 초기화
+    resetOnAll: state => {
+      state.selectedClosetIds = [];
     },
   },
 });
 
-export const { selectCloset } = selectClosetSlice.actions;
+export const { selectCloset, resetOnAll } = selectClosetSlice.actions;
 
 export default selectClosetSlice.reducer;
