576cb9b5edef6f9ed013f23a52fc953101591e2c
seoyoung81 <seosue0801@gmail.com>
2023-11-21 08:04:17 +0900

    Feat: 친구에게 추천 페이지 초기
    Former-commit-id: d65333b37d9877e64d7dd2002d61251b1436cc2c

---
+++ frontend/src/pages/RecommendedClothesPage.tsx
@@ -0,0 +1,13 @@
+import RecommenedFeed from "../components/recommended/molecules/RecommendedFeed";
+
+const RecommendedClothesPage = () => {
+    return (
+        <>  
+         친구가 나에게 착장 추천해준 페이지
+         이거 map 돌려야지
+         <RecommenedFeed />
+        </>
+    )
+}
+
+export default RecommendedClothesPage;
