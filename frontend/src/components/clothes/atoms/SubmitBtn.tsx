67a4f097d451aa7309823dd6d27e83b62fadfe42
seoyoung81 <seosue0801@gmail.com>
2023-11-21 11:05:54 +0900

    Feat: 친구 추천 옷 리덕스에 저장
    Former-commit-id: 5ee2e5ac7e94ab5f2964a76e92e5d1c03fda5076

--- frontend/src/components/clothes/atoms/SubmitBtn.tsx
+++ frontend/src/components/clothes/atoms/SubmitBtn.tsx
@@ -13,9 +13,7 @@ const SubmitBtn: React.FC<PropsType> = ({ handleSubmit }) => {
       <div className="flex w-full h-full items-center justify-center">
         <button
           className={
-            path === 'notmycloset'
-              ? 'hidden'
-              : 'bg-pink-hot w-[150px] text-center rounded-xl shadow-xl text-white text-AppBody1 p-4 tracking-[10px] font-bold mr-[150px]'
+            'bg-pink-hot w-[150px] text-center rounded-xl shadow-xl text-white text-AppBody1 p-4 tracking-[10px] font-bold mr-[150px]'
           }
           onClick={handleSubmit}>
           {path === 'notmycloset' ? '제안하기' : '제출하기'}
