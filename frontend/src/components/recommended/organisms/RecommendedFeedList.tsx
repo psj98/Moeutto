50921a358f73904645ea41a2d7ad4d599b2152d9
seoyoung81 <seosue0801@gmail.com>
2023-11-21 10:35:54 +0900

    Feat: 친구 추천 목록 UI
    Former-commit-id: 5649a0ed8ce05fb8985353fa8cb434e9a290a9b3

---
+++ frontend/src/components/recommended/organisms/RecommendedFeedList.tsx
@@ -0,0 +1,14 @@
+import RecommenedFeed from "../molecules/RecommendedFeed";
+
+const RecommendedFeedList = () => {
+    return (
+        <>  
+         <RecommenedFeed />
+         <RecommenedFeed />
+         <RecommenedFeed />
+         <RecommenedFeed />
+        </>
+    )
+}
+
+export default RecommendedFeedList;
--- frontend/src/pages/RecommendedClothesPage.tsx
+++ frontend/src/pages/RecommendedClothesPage.tsx
@@ -1,12 +1,10 @@
-import RecommenedFeed from "../components/recommended/molecules/RecommendedFeed";
+import RecommendedFeedList from "../components/recommended/organisms/RecommendedFeedList";
 
 const RecommendedClothesPage = () => {
     return (
-        <>  
-         친구가 나에게 착장 추천해준 페이지
-         이거 map 돌려야지
-         <RecommenedFeed />
-        </>
+        <div className="mt-12">  
+            <RecommendedFeedList />
+        </div>
     )
 }
 
