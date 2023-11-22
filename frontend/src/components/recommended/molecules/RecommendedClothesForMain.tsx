3ab9f17afa9dbde957a092539180a68ee429faf9
seoyoung81 <seosue0801@gmail.com>
2023-11-22 14:35:56 +0900

    Fix: 추천 멘트 안나오는 것 수정
    Former-commit-id: 2367c41f1ea62cd8965018993b0e36630d66f237

--- frontend/src/components/recommended/molecules/RecommendedClothesForMain.tsx
+++ frontend/src/components/recommended/molecules/RecommendedClothesForMain.tsx
@@ -49,7 +49,7 @@ const RecommenedClothesForMain = () => {
 
                 {/* 옷 */}
                 <div className="w-[90%] rounded-lg mx-auto flex justify-center px-2 pb-4">
-                    <div className='flex-wrap flex gap-2 justify-start px-auto mx-auto'>
+                    <div className='flex-wrap flex gap-2 justify-start'>
                         <img src='images/clothes1.png' alt="clohtes" className='w-1/2 h-auto max-w-[100px] max-h-[100px] object-cover bg-white rounded-xl' />
                         <img src='images/clothes2.png' alt="clohtes" className='w-1/2 h-auto max-w-[100px] max-h-[100px] object-cover bg-white rounded-xl' />
                         <img src='images/clothes3.png' alt="clohtes" className='w-1/2 h-auto max-w-[100px] max-h-[100px] object-cover bg-white rounded-xl' />
--- frontend/src/components/recommended/molecules/RecommendedFeed.tsx
+++ frontend/src/components/recommended/molecules/RecommendedFeed.tsx
@@ -50,10 +50,10 @@ const RecommenedFeed = ({ index, date, nickname, clothesList, comment }) => {
 
                 {/* 옷 */}
                 <div className="w-[90%] rounded-lg mx-auto flex justify-center px-6 pb-2">
-                    <div className='flex-wrap flex gap-4 justify-start'>
+                    <div className='flex-wrap flex gap-4 justify-start mb-4'>
                         {clothesList?.map((item) => {
                             return (
-                                <img src={item.image} alt="clohtes" className='w-[130px] h-[130px] object-cover bg-white rounded-xl' />
+                                <img src={item.image} alt="clohtes" className='w-1/2 min-w-[100px] min-h-[100px] max-w-[110px] max-h-[110px] object-cover bg-white rounded-xl' />
                             )
                         })}
                     </div>
