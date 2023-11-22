15e3236d4c8866b37871ecede19f066c00e4741c
seoyoung81 <seosue0801@gmail.com>
2023-11-22 09:49:22 +0900

    Design: 메인페이지 친구 추천 탭 수정
    Former-commit-id: aa5e63e401de1c758c2cd575edf7de2ed1bc9881

--- frontend/src/components/recommended/molecules/RecommendedClothesForMain.tsx
+++ frontend/src/components/recommended/molecules/RecommendedClothesForMain.tsx
@@ -1,39 +1,61 @@
-// import { PiCoatHangerBold } from 'react-icons/pi';
+import { PiCoatHangerBold } from 'react-icons/pi';
+import styled from 'styled-components';
+
+const Balloon = styled.div`
+    position:relative;
+    width:180px;
+    height:50px;
+    background: #F9F5F6;
+    border-radius: 10px;
+    z-index: 0;
+    margin-top: 4px;
+    padding: 10px;
+
+  &:after {
+    border-top:0px solid transparent;
+    border-left: 10px solid transparent;
+    border-right: 10px solid transparent;
+    border-bottom: 10px solid #F9F5F6;
+    content:"";
+    position:absolute;
+    top:-8px;
+    left:20px;
+  }
+`;
 
 const RecommenedClothesForMain = () => {
+
     return (
-        <div className="flex justify-center mb-4">
-            <div className="w-[95%] bg-pink rounded-xl relative">
+        <div className="flex justify-center mb-6">
+            <div className="w-[85%] bg-[#FDCEDF] shadow-md rounded-xl relative">
 
                 {/* 친구 정보 */}
                 <div className="flex items-end gap-4 p-4">
-                    {/* 프로필 사진 */}
-                    <img src="/images/dog.jpeg" alt="profile" className="w-[60px] h-[60px] rounded-full object-cover" />
                     <div>
-                        <div className="text-AppBody2">2023.08.01</div>
+                        <div className="text-AppBody2">2023.11.12</div>
                         {/* 닉네임 */}
-                        <div className="text-AppBody1">
-                            모드리치
-                            <span className="text-white">님의 추천</span>
+                        <div className="text-AppBody1 relative">
+                            <span className='z-99'>모으또</span>
+                            <span className="text-pink-hot">님의 추천</span>
+                         
+                                <Balloon>이렇게도 입어봐!</Balloon>
+                      
                         </div>
                     </div>
                 </div>
-
-                {/* 옷 */}
-                <div className="w-[90%] h-[80px] rounded-lg mx-auto relative">
-                    {/* <div className='absolute -top-16 right-0'>
-                        <PiCoatHangerBold size={60} className='text-pink-hot' /> 
-                    </div> */}
-                    <div className='flex-wrap flex gap-3 pt-2 justify-center'>
-                        <img src="/images/clothes1.png" alt="clohtes1" className='w-[65px] h-[65px] object-cover bg-white rounded-xl' />
-                        <img src="/images/clothes1.png" alt="clohtes1" className='w-[65px] h-[65px] object-cover bg-white rounded-xl' />
-                        <img src="/images/clothes1.png" alt="clohtes1" className='w-[65px] h-[65px] object-cover bg-white rounded-xl' />
-                        <img src="/images/clothes1.png" alt="clohtes1" className='w-[65px] h-[65px] object-cover bg-white rounded-xl' />
-                    </div>
+                <div className='absolute top-4 right-4'>
+                    <PiCoatHangerBold size={60} className='text-pink-hot' /> 
                 </div>
 
-                {/* 내용 너무 길어도 이상하니까 20자 정도로 제한 */}
-                <div className='ms-[7%] max-w-[90%] my-2'>이렇게 입어보는 것이 어떻겠니?</div>
+                {/* 옷 */}
+                <div className="w-[90%] rounded-lg mx-auto flex justify-center px-6 pb-2">
+                    <div className='flex-wrap flex gap-4 justify-start'>
+                        <img src='images/clothes1.png' alt="clohtes" className='w-[100px] h-[100px] object-cover bg-white rounded-xl' />
+                        <img src='images/clothes2.png' alt="clohtes" className='w-[100px] h-[100px] object-cover bg-white rounded-xl' />
+                        <img src='images/clothes3.png' alt="clohtes" className='w-[100px] h-[100px] object-cover bg-white rounded-xl' />
+                        <img src='images/clothes4.png' alt="clohtes" className='w-[100px] h-[100px] object-cover bg-white rounded-xl' />
+                    </div>
+                </div>
             </div>
         </div>
     )
