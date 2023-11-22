49f48d3283b35d3de1f9a743659a2685b3cfa5b7
seoyoung81 <seosue0801@gmail.com>
2023-11-22 10:45:03 +0900

    Design: 추천 받은 옷이 없는 경우
    Former-commit-id: aaefecbd08667382ed2c97113439253167785021

--- frontend/src/components/recommended/organisms/RecommendedFeedList.tsx
+++ frontend/src/components/recommended/organisms/RecommendedFeedList.tsx
@@ -5,6 +5,7 @@ import { authInstance } from "../../../api/api";
 
 const RecommendedFeedList = () => {
     const [recommendFeedListData, setRecommendFeedListData] = useState<any>();
+    const [isLoading, setIsLoading] = useState<boolean>(false);
 
     const getFriendsRecommendList = async () => {
         try {
@@ -12,6 +13,9 @@ const RecommendedFeedList = () => {
             const response = await axiosInstance.get('friend-outfits');
         
             setRecommendFeedListData(response.data.data);
+            if (response.data.data) {
+                setIsLoading(true);
+            }
          
         } catch (error) {
             console.log('친구에게 옷 추천 받은 목록 조회 실패', error)
@@ -24,18 +28,24 @@ const RecommendedFeedList = () => {
 
     return (
         <>
-            {recommendFeedListData?.map((item, index) => {
-                return (
-                    <RecommenedFeed 
-                        key={index}
-                        index={index}
-                        date={item.regDate}
-                        nickname={item.recommenderNickname}
-                        clothesList={item.clothesList} 
-                        comment={item.comment}
-                    />
+            {isLoading ? (
+                recommendFeedListData?.map((item, index) => {
+                    return (
+                        <RecommenedFeed 
+                            key={index}
+                            index={index}
+                            date={item.regDate}
+                            nickname={item.recommenderNickname}
+                            clothesList={item.clothesList} 
+                            comment={item.comment}
+                        />
+                    )
+                })) : (
+                    <div className="flex justify-center">
+                        <div className="flex justify-center">아직 추천 받은 옷이 없어요 😢</div>
+                    </div>
                 )
-            })}
+        }
         </>
     )
 }
