b111562a319c5fb7fc9bba30333ad920844c9257
s01k1m <sk618dev@gmail.com>
2023-11-23 08:09:45 +0900

    Design: 버튼 작업중
    Former-commit-id: 5e773d2729ed50d891f9f00d0c450993839f1526

--- frontend/src/components/clothes/atoms/SubmitBtn.tsx
+++ frontend/src/components/clothes/atoms/SubmitBtn.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import Button from '@mui/material/Button';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 interface PropsType {
   handleSubmit: React.MouseEventHandler<HTMLButtonElement>;
 }
+
+const themeLight = createTheme({
+  palette: {
+    background: {
+      default: '#fff',
+    },
+  },
+});
 
 const SubmitBtn: React.FC<PropsType> = ({ handleSubmit }) => {
   const pathname = window.location.href;
@@ -11,13 +21,19 @@ const SubmitBtn: React.FC<PropsType> = ({ handleSubmit }) => {
   return (
     <>
       <div className="flex w-full h-full items-center justify-center">
-        <button
-          className={
-            'bg-pink-hot w-[150px] text-center rounded-xl shadow-xl text-white text-AppBody1 p-4 tracking-[10px] font-bold mr-[150px]'
-          }
-          onClick={handleSubmit}>
-          {path === 'notmycloset' ? '제안하기' : '제출하기'}
-        </button>
+        <ThemeProvider theme={themeLight}>
+          <button
+            className={
+              'bg-pink-hot w-[150px] text-center rounded-xl shadow-xl text-white text-AppBody1 p-4 tracking-[10px] font-bold mr-[150px]'
+            }
+            onClick={handleSubmit}></button>
+          <Button
+            variant="contained"
+            // color="success"
+            className="bg-pink-hot w-[150px] text-center rounded-xl shadow-xl text-white text-AppBody1 p-4 tracking-[10px] font-bold mr-[150px]">
+            {path === 'notmycloset' ? '제안하기' : '제출하기'}
+          </Button>
+        </ThemeProvider>
       </div>
     </>
   );
