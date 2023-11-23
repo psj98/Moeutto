a1a291cc919f55f2badbd68a90b4be9dd6a666e8
s01k1m <sk618dev@gmail.com>
2023-11-23 09:49:44 +0900

    Design: 버튼 클릭시 텍스트 효과
    Former-commit-id: df799e039d4d6c6e84ae985886b5f6b3f5492ef3

--- frontend/src/components/clothes/atoms/SubmitBtn.tsx
+++ frontend/src/components/clothes/atoms/SubmitBtn.tsx
@@ -13,7 +13,7 @@ const SubmitBtn: React.FC<PropsType> = ({ handleSubmit }) => {
       <div className="flex w-full h-full items-center justify-center">
         <button
           className={
-            'bg-pink-hot w-[150px] text-center rounded-xl shadow-xl text-white text-AppBody1 p-4 tracking-[10px] font-bold mr-[150px]'
+            'bg-pink-hot w-[150px] text-center rounded-xl shadow-xl hover:text-black focus:text-black text-white text-AppBody1 p-4 pe-3 tracking-[5px] font-bold mr-[150px]'
           }
           onClick={handleSubmit}>
           {path === 'notmycloset' ? '제안하기' : '제출하기'}
